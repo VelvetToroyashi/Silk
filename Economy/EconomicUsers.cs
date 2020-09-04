501ba7ed29c5dc25cea7f41a8a9668759530d8e7
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2020-09-04 19:55:20 -0400

    Reeee

---
+++ Economy/EconomicUsers.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using SilkBot.ServerConfigurations;
+using System.Collections.Concurrent;
+
+namespace SilkBot.Economy
+{
+    public sealed class EconomicUsers
+    {
+        public ConcurrentDictionary<ulong, DiscordEconomicUser> Users { get; } = new ConcurrentDictionary<ulong, DiscordEconomicUser>();
+        public static EconomicUsers Instance { get; } = new EconomicUsers();
+        public void Add(DiscordMember member)
+        {
+            var Id = member.Id;
+            var user = new DiscordEconomicUser(Id, member.DisplayName);
+            if (Users.ContainsKey(Id)) return;
+            Users.TryAdd(Id, user);
+            Bot.GlobalConfig.EconomicUsers.Add(user); 
+        }
+        //I don't use this as much as I could and should. Sad.//
+        public bool UserExists(ulong Id) => Users.ContainsKey(Id);
+
+        private EconomicUsers() { }
+    }
+}
