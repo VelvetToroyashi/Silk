56eb3120612a2b42db1900b6c037478e44750c4b
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2020-10-15 17:50:25 -0400

    Fixed GuildJoinHandler

--- Commands/Bot/GuildJoinHandler.cs
+++ Commands/Bot/GuildJoinHandler.cs
@@ -13,7 +13,7 @@ namespace SilkBot.Commands.Bot
     {
         public GuildJoinHandler() => Instance.Client.GuildCreated += OnGuildJoin;
 
-        private async Task OnGuildJoin(GuildCreateEventArgs e)
+        private async Task OnGuildJoin(DiscordClient c, GuildCreateEventArgs e)
         {
             var allChannels = (await e.Guild.GetChannelsAsync()).OrderBy(channel => channel.Position);
             var botAsMember = await e.Guild.GetMemberAsync(Instance.Client.CurrentUser.Id);
@@ -25,18 +25,18 @@ namespace SilkBot.Commands.Bot
             var embed = new DiscordEmbedBuilder()
                 .WithTitle("Thank you for adding me!")
                 .WithColor(new DiscordColor("94f8ff"))
-                .WithThumbnail(e.Client.CurrentUser.AvatarUrl)
-                .WithFooter("Silk!", e.Client.CurrentUser.AvatarUrl)
+                .WithThumbnail(c.CurrentUser.AvatarUrl)
+                .WithFooter("Silk!", c.CurrentUser.AvatarUrl)
                 .WithTimestamp(DateTime.Now);
 
             var sb = new StringBuilder();
-            sb.Append("Thank you for choosing Silk! to join your server <3\n")
-                .Append("I am a relatively lightweight bot with many functions - partially in moderation, ")
-                .Append("partially in games, with many more features to come!\n")
+            sb.Append("Thank you for choosing Silk! to join your server <3")
+                .AppendLine("I am a relatively lightweight bot with many functions - partially in moderation, ")
+                .AppendLine("partially in games, with many more features to come!")
                 .Append("If there's an issue, feel free to [Open an issue on GitHub](https://github.com/VelvetThePanda/Silkbot/issues), ")
-                .Append("or if you're not familiar with GitHub, feel free\n")
-                .Append("to message the developers directly via [p]support <your message>, where `[p]` is the prefix.\n")
-                .Append($"By default, the prefix is `{SilkDefaultCommandPrefix}`, or <@{e.Client.CurrentUser.Id}>, but this can be changed by [p]prefix <your prefix here>.");
+                .AppendLine("or if you're not familiar with GitHub, feel free")
+                .AppendLine("to message the developers directly via [p]support <your message>, where `[p]` is the prefix.")
+                .Append($"By default, the prefix is `{SilkDefaultCommandPrefix}`, or <@{c.CurrentUser.Id}>, but this can be changed by [p]prefix <your prefix here>.");
 
             embed.WithDescription(sb.ToString());
 
