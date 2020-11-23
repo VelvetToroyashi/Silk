bd52224c6ae77ceb6e1fb8688d4704254f265dd7
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2020-11-23 11:13:19 -0500

    Removed it ~ Velvet

--- Silk_Core/Utilities/BotConfig.cs
+++ Silk_Core/Utilities/BotConfig.cs
@@ -4,19 +4,13 @@ namespace SilkBot.Utilities
 {
     public record BotConfig
     {
-        // Not quite permenant. //
-        // I'll probably end up removing this, actually. It doesn't serve much purpose ~Velvet //
-        internal string Token           { get; init; }
-        internal string Connection      { get; init; }
-        
+        // Took them out; they make no sense. //
         internal string e621APIKey      { get; init; }
         internal string e621Username    { get; init; }
         
 
         public BotConfig(IConfiguration c)
         {
-            Connection = c.GetConnectionString("DbConnection");
-            Token = c.GetConnectionString("Token");
             e621APIKey = c.GetSection("API_Keys")["e621_Key"];
             e621Username = c.GetSection("API_Keys")["e621_User"];
         }
