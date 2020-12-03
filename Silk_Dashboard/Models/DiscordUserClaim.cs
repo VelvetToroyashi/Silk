1c39f19800c483f3e72b816b2cdc88e6b0a5a963
Caleb Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2020-12-03 05:44:26 -0500

    Added Blazor project + setup base structure
    Note: Follow the 'Setup' section to configure your Discord App settings - https://github.com/PassiveModding/BlazorDiscordOAuth2 (I used port 5001, so if you change, make sure to update 'launchSettings.json' to reflect change)
    
    - Setup Discord OAuth for Blazor using reference project (credit given in 'readme.md' in sub project)
    - Setup Darkly theme from Bootswatch (works well so far, think there maybe some 'gotchas' but so far so good)
    - Customized AccountManage page for displaying user's Guilds

---
+++ Silk_Dashboard/Models/DiscordUserClaim.cs
@@ -0,0 +1,20 @@
+﻿namespace Silk_Dashboard.Models
+{
+    public class DiscordUserClaim
+    {
+        public ulong UserId { get; set; }
+        public string Name { get; set; }
+        public string Discriminator { get; set; }
+        public string Avatar { get; set; }
+
+        /// <summary>
+        /// Will be null if the email scope is not provided
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Whether the email on this account has been verified, can be null
+        /// </summary>
+        public bool? Verified { get; set; }
+    }
+}
