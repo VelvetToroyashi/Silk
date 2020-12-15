06e37c733be0969f8ac9a1bf89de390687b98256
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2020-12-15 06:01:06 -0500

    Removed unused exceptions classes/namespace

--- Silk_Core/Commands/Furry/Utilities/eBooruBaseCommand.cs
+++ Silk_Core/Commands/Furry/Utilities/eBooruBaseCommand.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Buffers.Text;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
