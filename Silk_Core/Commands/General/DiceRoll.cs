f5a7b03626f97a913e213038cfc60edad6840702
Alex-ABWorld <alexbu2015@hotmail.com>
2020-12-17 22:22:30 +0000

    Write Dice Parser + Add Text Parsing Utility

--- Silk_Core/Commands/General/DiceRoll.cs
+++ Silk_Core/Commands/General/DiceRoll.cs
@@ -16,56 +16,68 @@ namespace SilkBot.Commands.General
     [Category(Categories.General)]
     public class DiceRoll : BaseCommandModule
     {
+        struct Dice
+        {
+            public int TotalNumber;
+            public int NoSides;
+        }
+
         [Command]
         public async Task Random(CommandContext ctx, int max = 100) =>
             await ctx.RespondAsync(new Random().Next(max).ToString()).ConfigureAwait(false);
 
-
-        private enum State
-        {
-            DiceMult,
-            DiceSign,
-            DiceSide,
-            Unknown,
-            DiceAddi,
-            Modifier
-        }
-        
-        
-        //I'll get back to this later :c
         [Command]
         public async Task Roll(CommandContext ctx, [RemainingText] string roll)
         {
-            // if (roll.Any(r => r is >= '0' and <= '9' and not 'd' or '+' or ' '))
-            //     throw new ArgumentException("That doesn't seem to be a valid roll..");
-            
-            State lastState = State.Unknown;
+            var parser = new DiceParser(roll);
+            var allDice = parser.Run();
 
-            List<State> stateList = new();
-            
-            for (int i = 0; i < roll.Length; i++)
+            // Test printing:
+            var sb = new StringBuilder();
+            for (int i = 0; i < allDice.Count; i++)
             {
-                lastState = roll[i] switch
-                {
-                    '+'                                               => State.DiceAddi,
-                    'd'                                               => State.DiceSign,
-                    >= '1' or <= '0' when lastState is State.DiceAddi => State.Modifier,
-                    >= '1' or <= '0' when lastState is State.Unknown  => State.DiceMult,
-                    >= '1' or <= '0' when lastState is State.DiceSide => State.DiceSide,
-                    >= '1' or <= '0' when lastState is State.DiceSign => State.DiceSide,
-                    >= '1' or <= '0' when lastState is State.DiceMult => State.DiceMult,
-                    _                                                 => State.Unknown
-                };
-                stateList.Add(lastState);
+                sb.Append(allDice[i].TotalNumber);
+                sb.Append(" x ");
+                sb.Append(allDice[i].NoSides);
+                sb.Append(" sides");
+
+                if (i < allDice.Count - 1) sb.Append(", ");
             }
 
-            await ctx.RespondAsync(
-                         stateList
-                             .GroupBy(i => i)
-                             .Select(s => $"{s.Key} ({s.Count()} times)")
-                             .JoinString("\n"))
-                             .ConfigureAwait(false);
+            await ctx.RespondAsync(sb.ToString()).ConfigureAwait(false);
+        }
 
+        class DiceParser : TextParser
+        {
+            public DiceParser(string text) : base(text) { }
+
+            public IList<Dice> Run()
+            {
+                var result = new List<Dice>();
+
+                // Read all the dice.
+                do result.Add(ParseDice());
+                while (ReadIf('+'));
+
+                // Ensure there's no junk data at the end.
+                if (Read() != EOT) throw new Exception($"Unexpected character {CurrentPosition}");
+
+                return result;
+            }
+
+            public Dice ParseDice()
+            {
+                var res = new Dice();
+
+                res.TotalNumber = ReadNumberOr1();
+
+                // Make sure there's definitely a "d" in the middle.
+                if (!ReadIf('d')) throw new Exception($"Unexpected character at position {CurrentPosition}!");
+
+                res.NoSides = ReadNumberOr1();
+
+                return res;
+            }
         }
     }
 }
