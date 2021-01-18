353ec08161a63c6fcc61347dbabb3c473c100ee0
Caleb Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2021-01-18 02:12:41 -0500

    Added initial go at baltop command for issue #129

---
+++ src/Silk.Core/Commands/Economy/HighRollerCommand.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Microsoft.EntityFrameworkCore;
+using Silk.Core.Database;
+using Silk.Core.Database.Models;
+using Silk.Core.Utilities;
+
+namespace Silk.Core.Commands.Economy
+{
+    [Category(Categories.Economy)]
+    public class HighRollerCommand : BaseCommandModule
+    {
+        private readonly IDbContextFactory<SilkDbContext> _dbFactory;
+
+        public HighRollerCommand(IDbContextFactory<SilkDbContext> dbContextFactory)
+        {
+            _dbFactory = dbContextFactory;
+        }
+
+        [Command("baltop")]
+        [Aliases("highroller")]
+        [Description("See which members have the most money in the Guild!")]
+        public async Task BalTop(CommandContext ctx)
+        {
+            SilkDbContext db = _dbFactory.CreateDbContext();
+            List<GlobalUserModel> economyUsers = db.GlobalUsers
+                .OrderByDescending(user => user.Cash)
+                .Take(10)
+                .ToList();
+
+            if (!economyUsers.Any())
+            {
+                await ctx.RespondAsync($"Seems you don't have any members with economy accounts. Ask some members to use `{ctx.Prefix}daily` and I'll set up an account for them *:)*");
+                return;
+            }
+
+            DiscordEmbedBuilder embed = new DiscordEmbedBuilder()
+                .WithTitle("Members with the Highest Economy Balances! :money_with_wings:")
+                .WithColor(DiscordColor.Gold);
+
+            for (var i = 0; i < economyUsers.Count; ++i)
+            {
+                var position = i + 1;
+                var economyUser = economyUsers[i];
+                var displayName = ctx.Guild.Members[economyUser.Id].DisplayName;
+                
+                embed.AddField(i == 0 ? $"{position}. :crown:" : $"{position}. ", $"{displayName}", true);
+            }
+
+            await ctx.RespondAsync(embed: embed);
+        }
+    }
+}
