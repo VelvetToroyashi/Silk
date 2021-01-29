50e9fac29e178879316c8fd41b57a2debca13eaa
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-01-29 01:21:37 -0500

    Removed redudant check

--- src/Silk.Core/Commands/General/ClearCommand.cs
+++ src/Silk.Core/Commands/General/ClearCommand.cs
@@ -17,16 +17,6 @@ namespace Silk.Core.Commands.General
         [Description("Cleans all messages from all users.")]
         public async Task Clear(CommandContext ctx, int messages = 5)
         {
-            // Anyone who's got permission to manage channels might not be staff, so adding [RequireFlag(UserFlag.Staff)] needlessly permwalls it. //
-            if (!ctx.Member.HasPermission(Permissions.ManageMessages))
-            {
-                await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
-                    .WithAuthor(ctx.Member.DisplayName, null, ctx.Member.AvatarUrl)
-                    .WithColor(DiscordColor.Red)
-                    .WithDescription("Sorry, but you need to be able to manage messages to use this command!"));
-                return;
-            }
-
             IReadOnlyList<DiscordMessage> queriedMessages = await ctx.Channel.GetMessagesAsync(messages + 1);
             await ctx.Channel.DeleteMessagesAsync(queriedMessages, $"{ctx.User.Username}{ctx.User.Discriminator} called clear command.");
             
