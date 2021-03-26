a25b829926bba6de34b59cb0a40623dfc747e882
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-03-25 21:46:44 -0400

    Documentation. Reeeeee.

--- src/Silk.Core.Data/MediatR/Unified/Reminders/ReminderGetAllRequest.cs
+++ src/Silk.Core.Data/MediatR/Unified/Reminders/ReminderGetAllRequest.cs
@@ -7,8 +7,14 @@ using Silk.Core.Data.Models;
 
 namespace Silk.Core.Data.MediatR.Unified.Reminders
 {
+    /// <summary>
+    /// Gets all reminders.
+    /// </summary>
     public record GetAllRemindersRequest : IRequest<IEnumerable<Reminder>>;
 
+    /// <summary>
+    /// The default handler for <see cref="GetAllRemindersRequest"/>.
+    /// </summary>
     public class GetAllRemindersHandler : IRequestHandler<ReminderRequest.GetAll, IEnumerable<Reminder>>
     {
         private readonly GuildContext _db;
