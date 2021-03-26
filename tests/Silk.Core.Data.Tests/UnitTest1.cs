40d6f01761af4b7e63fa9c8f4b462dfc1bd1d2ba
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-03-26 19:14:50 -0400

    Remove Silk.Data and add Silk.Core.Data.Tests.

---
+++ tests/Silk.Core.Data.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace Silk.Core.Data.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
