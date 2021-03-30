0d7798275a763609ca2fa8a326040cb12d0c1b0c
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-03-30 11:49:03 -0400

    One of those vague commit messages

--- src/Silk.Extensions/DSharpPlus/DiscordMessageExtensions.cs
+++ src/Silk.Extensions/DSharpPlus/DiscordMessageExtensions.cs
@@ -21,24 +21,5 @@ namespace Silk.Extensions.DSharpPlus
             DiscordChannel channel = messageCollection.First().Channel;
             await channel.DeleteMessagesAsync(messageCollection);
         }
-
-        public static async IAsyncEnumerator<DiscordMessage> GetAsyncEnumerator(this DiscordChannel channel)
-        {
-            bool hasMorePages = true;
-            DiscordMessage last = null;
-            do
-            {
-                var messages = last != null ? await channel.GetMessagesBeforeAsync(last.Id) : await channel.GetMessagesAsync();
-                if (messages.Count < 100)
-                    hasMorePages = false;
-
-                foreach (var message in messages)
-                {
-                    last = message;
-                    yield return message;
-                }
-
-            } while (hasMorePages);
-        }
     }
 }
--- src/Silk.Shared/Abstractions/DSharpPlus/Interfaces/IChannel.cs
+++ src/Silk.Shared/Abstractions/DSharpPlus/Interfaces/IChannel.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using DSharpPlus.Entities;
 
 namespace Silk.Shared.Abstractions.DSharpPlus.Interfaces
 {
+    /// <summary>
+    /// Represents a mockable abstraction of a <see cref="DiscordChannel"/>.
+    /// </summary>
     public interface IChannel
     {
+        /// <summary>
+        /// The id of the current channel.
+        /// </summary>
         public ulong Id { get; init; }
-        public IGuild Guild { get; init; }
+
+        /// <summary>
+        /// What guild this channel belongs to, if any.
+        /// </summary>
+        public IGuild? Guild { get; init; }
+
+        /// <summary>
+        /// The messages sent in this channel.
+        /// </summary>
         public IReadOnlyList<IMessage> Messages { get; init; }
 
+        /// <summary>
+        /// Sends an <see cref="IMessage"/> to the channel.
+        /// </summary>
+        /// <param name="message">The message to send to the channel.</param>
+        /// <returns>The sent <see cref="IMessage"/>.</returns>
         public Task<IMessage> SendAsync(IMessage message);
+
+        /// <summary>
+        /// Gets a specific message from the channel.
+        /// </summary>
+        /// <param name="id">The Id of the message to retrieve.</param>
+        /// <returns>An <see cref="IMessage"/> if the message exists, otherwise null.</returns>
         public Task<IMessage?> GetMessageAsync(ulong id);
-
-
-        //public static implicit operator IChannel(DiscordChannel channel) => (IChannel)new Channel(channel.Id, channel.Guild, channel.Messages);
     }
 }
