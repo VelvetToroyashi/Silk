9b28c2f864ffb8526a197844786896b68f6ff0d4
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-04-14 18:23:56 -0400

    Small cleanup

--- src/Silk.Core.Discord/Commands/Server/Config/ConfigCommand.cs
+++ src/Silk.Core.Discord/Commands/Server/Config/ConfigCommand.cs
@@ -2,26 +2,15 @@
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using Silk.Core.Data.Models;
-using Silk.Core.Discord.Services.Interfaces;
 using Silk.Core.Discord.Utilities;
-using Silk.Shared.Abstractions.DSharpPlus.Interfaces;
 
 namespace Silk.Core.Discord.Commands.Server.Config
 {
     [RequireGuild]
     [Group("config")]
     [RequireFlag(UserFlag.Staff)]
-    public partial class ConfigCommand : BaseCommandModule
+    public sealed partial class ConfigCommand : BaseCommandModule
     {
-        private readonly IMessageSender _sender;
-        private readonly IInputService _input;
-
-        public ConfigCommand(IMessageSender sender, IInputService input)
-        {
-            _sender = sender;
-            _input = input;
-        }
-
         [GroupCommand]
         public async Task ConfigWrapper(CommandContext ctx) =>
             await ctx.RespondAsync($"See `{ctx.Prefix}help config` instead.");
