0586dec7d7d570802471f6e113a2812d24e48ae4
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-04-29 20:19:57 -0400

    Add channel cache

--- src/Silk.Shared/Abstractions/DSharpPlus/Concrete/Channel.cs
+++ src/Silk.Shared/Abstractions/DSharpPlus/Concrete/Channel.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using DSharpPlus.Entities;
 using Silk.Shared.Abstractions.DSharpPlus.Interfaces;
 
@@ -14,10 +16,19 @@ namespace Silk.Shared.Abstractions.DSharpPlus.Concrete
 
         private readonly DiscordChannel _channel;
 
-        private Channel(DiscordChannel channel) => _channel = channel;
+        private Channel(DiscordChannel channel, bool caching)
+        {
+            _channel = channel;
 
+            if (!caching && !IsPrivate)
+            {
+                var guild = Concrete.Guild.GuildsCache[_channel.Guild.Id];
+                if (guild.Channels.All(c => c.Id != Id))
+                    (guild.Channels as List<Channel>)!.Add(this);
+            }
+        }
         public async Task<IMessage?> GetMessageAsync(ulong id) => (Message?) await _channel.GetMessageAsync(id);
 
-        public static implicit operator Channel(DiscordChannel channel) => new(channel);
+        public static implicit operator Channel(DiscordChannel channel) => new(channel, false);
     }
 }
