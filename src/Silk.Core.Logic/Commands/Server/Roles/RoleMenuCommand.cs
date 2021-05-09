6854327f85268497af68c50301c98eaafd876ee9
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-05-09 18:08:48 -0400

    Send help

--- src/Silk.Core.Logic/Commands/Server/Roles/RoleMenuCommand.cs
+++ src/Silk.Core.Logic/Commands/Server/Roles/RoleMenuCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using DSharpPlus;
@@ -7,6 +8,7 @@ using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using MediatR;
 using Silk.Core.Data.MediatR.Guilds;
+using Silk.Extensions.DSharpPlus;
 using Silk.Shared.Constants;
 
 namespace Silk.Core.Logic.Commands.Server.Roles
@@ -18,19 +20,18 @@ namespace Silk.Core.Logic.Commands.Server.Roles
     [ModuleLifespan(ModuleLifespan.Transient)] // We're gonna hold some states. //
     public class RoleMenuCommand : BaseCommandModule
     {
-        private readonly Regex EmojiRegex = new(@"<a?:(.+):([0-9]+)>");
+        private readonly IMediator _mediator;
+        private readonly Regex _comboRegex = new(@"((<a?(:.+:)([0-9]+)>).?(<@&[0-9]+>))");
 
-        private IMediator _mediator;
-        public RoleMenuCommand(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+        public RoleMenuCommand(IMediator mediator) => _mediator = mediator;
 
         [Command]
-        [Description("Automagically configure a role menu based on a message! Must provide message link!")]
+        [Description("Automagically configure a role menu based on a message! Must provide message link!\n Supported format: `<emoji> @Role` \n`<emoji> @Role`")]
         [RequireBotPermissions(Permissions.ManageRoles)]
         public async Task Create(CommandContext ctx, DiscordMessage messageLink)
         {
+            var message = messageLink;
+            var matches = _comboRegex.Matches(message.Content);
             var config = await _mediator.Send(new GetGuildConfigRequest(ctx.Guild.Id));
 
             if (config.ReactionRoles.Any(r => r.MessageId == messageLink.Id))
@@ -39,39 +40,43 @@ namespace Silk.Core.Logic.Commands.Server.Roles
                 return;
             }
 
+            DiscordEmoji loading = DiscordEmoji.FromGuildEmote(ctx.Client, Emojis.LoadingId);
+            DiscordEmoji failed = DiscordEmoji.FromGuildEmote(ctx.Client, Emojis.DeclineId);
 
-            var roles = messageLink.MentionedRoles;
-            var waitMessage = await ctx.RespondAsync("Got it! This may take a few seconds.");
-            var loading = DiscordEmoji.FromGuildEmote(ctx.Client, Emojis.LoadingId);
-            var failed = DiscordEmoji.FromGuildEmote(ctx.Client, Emojis.DeclineId);
-            DiscordMessage progress = await waitMessage.RespondAsync(loading.ToString());
+            await ctx.RespondAsync("Got it! This should only take a few seconds.");
 
-            MatchCollection matches = EmojiRegex.Matches(messageLink.Content);
+            DiscordMessageBuilder progressMessageBuilder = new();
+            progressMessageBuilder.WithContent($"{loading} Checking message...").WithReply(0).WithoutMentions();
+            DiscordMessage progressMessage = await ctx.RespondAsync(progressMessageBuilder);
 
-            if (matches.Count is 0)
+            await Task.Delay(1000);
+            var missingReactions = new List<string>();
+
+            for (int i = 0; i < matches.Count; i++)
             {
-                await progress.DeleteAsync();
-                await waitMessage.ModifyAsync("There don't seem to be any emojis in that message!");
-                return;
+                if (message.Reactions.All(r => r.Emoji.Name != matches[i].Groups[3].Value))
+                {
+                    string emojiName = matches[i].Groups[3].Value;
+                    missingReactions.Add(emojiName);
+                    progressMessageBuilder.WithContent($"{failed} {emojiName} is missing it's reaction! Skipping.");
+                    await progressMessageBuilder.ModifyAsync(progressMessage);
+                    await Task.Delay(500);
+                    continue;
+                }
+
+                progressMessageBuilder.WithContent($"I'll give people {matches[i].Groups[4].Value} when they react with {matches[i].Groups[2].Value}!");
+                await progressMessageBuilder.ModifyAsync(progressMessage);
+                await Task.Delay(400);
+
             }
 
-            await progress.ModifyAsync($"{loading} Found {matches.Count} emojis...");
-            await Task.Delay(400);
 
-            // Ensure every role has a matching emoji. //
-            if (messageLink.MentionedRoles.Count > matches.Count)
-            {
-                await progress.ModifyAsync($"{failed} : Mismatched emoji and role count!");
-                return;
-            }
 
-            var emojis = new ulong[matches.Count];
 
-            for (int i = 0; i < messageLink.MentionedRoles.Count; i++)
-            {
 
-                await Task.Delay(700);
-            }
+
+
+
         }
     }
 }
