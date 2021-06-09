10a40855ff8e0f124b9d375e8d22cbb8a653f688
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-06-09 13:05:50 -0400

    Fix static ref

--- src/Silk.Core/EventHandlers/Guilds/GuildEventHandlers.cs
+++ src/Silk.Core/EventHandlers/Guilds/GuildEventHandlers.cs
@@ -10,7 +10,7 @@ namespace Silk.Core.EventHandlers.Guilds
         public GuildEventHandlers(GuildEventHandlerService guildHandler) => _guildHandler = guildHandler;
 
         public async Task OnGuildJoin(DiscordClient client, GuildCreateEventArgs args) =>
-            _guildHandler.CacheQueue.Enqueue(new(GuildEventHandlerService.JoinedGuild(args)));
+            _guildHandler.CacheQueue.Enqueue(new(_guildHandler.JoinedGuild(args)));
 
         public async Task OnGuildAvailable(DiscordClient client, GuildCreateEventArgs args) =>
             _guildHandler.CacheQueue.Enqueue(new(() => _guildHandler.CacheGuildAsync(args.Guild, client.ShardId)));
