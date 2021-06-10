cebcad0afbd12070616ada15a601acade51f155c
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-06-09 21:10:44 -0400

    Extension method.

---
+++ src/Silk.Extensions/DSharpPlus/SlashCommandExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.SlashCommands;
+
+namespace Silk.Extensions.DSharpPlus
+{
+	public static class SlashCommandExtensions
+	{
+		public static Task CreateThinkingResponseAsync(this InteractionContext ctx)
+			=> ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, new() {IsEphemeral = true});
+	}
+}
