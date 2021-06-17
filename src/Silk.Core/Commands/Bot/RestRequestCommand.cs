0e2a57042434ebad516dd3ec7ae969870b57eede
Caleb Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2021-06-17 03:41:03 -0400

    Use SilkConfigurationOptions to grab BotToken for use when making Api requests + fixed small spelling

--- src/Silk.Core/Commands/Bot/RestRequestCommand.cs
+++ src/Silk.Core/Commands/Bot/RestRequestCommand.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
@@ -8,10 +7,11 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using Humanizer;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Silk.Core.Utilities;
 using Silk.Extensions;
+using Silk.Shared.Configuration;
 
 namespace Silk.Core.Commands.Bot
 {
@@ -19,9 +19,12 @@ namespace Silk.Core.Commands.Bot
     {
         private const string baseUri = "https://discord.com/api/v9";
         private readonly IHttpClientFactory _clientFactory;
-        public RestRequestCommand(IHttpClientFactory clientFactory)
+        private readonly SilkConfigurationOptions _silkConfiguration;
+
+        public RestRequestCommand(IHttpClientFactory clientFactory, IOptions<SilkConfigurationOptions> options)
         {
             _clientFactory = clientFactory;
+            _silkConfiguration = options.Value;
         }
 
         [Command]
@@ -43,10 +46,6 @@ namespace Silk.Core.Commands.Bot
                 _ => throw new ArgumentException("Invalid HTTP method.")
             };
 
-            var token = typeof(DiscordConfiguration)
-                .GetField("_token", BindingFlags.NonPublic | BindingFlags.Instance)!
-                .GetValue(DiscordConfigurations.Discord) as string;
-
             var message = new HttpRequestMessage
             {
                 RequestUri = new(baseUri + uri),
@@ -54,26 +53,25 @@ namespace Silk.Core.Commands.Bot
                 Content = new StringContent(json ?? "", Encoding.UTF8, "application/json"),
             };
 
-
-            client.DefaultRequestHeaders.Add("Authorization", $"Bot {token}");
+            client.DefaultRequestHeaders.Add("Authorization", $"Bot {_silkConfiguration.Discord.BotToken}");
             client.DefaultRequestHeaders.Add("Accept", "application/json");
 
             HttpResponseMessage? res = await client.SendAsync(message);
             var jsn = JToken.Parse(await res.Content.ReadAsStringAsync() ?? "{}").ToString(Formatting.Indented);
 
             if (jsn.Length > 1000)
-                await new
-                        DiscordMessageBuilder()
-                    .WithContent($"{(int) res.StatusCode} - {res.StatusCode.Humanize(LetterCasing.AllCaps)}\n(Response was > 1000 characters. I've attatched the output to a file for you)")
+            {
+                await new DiscordMessageBuilder()
+                    .WithContent($"{(int)res.StatusCode} - {res.StatusCode.Humanize(LetterCasing.AllCaps)}\n(Response was > 1000 characters. I've attached the output to a file for you)")
                     .WithFile("response.json", jsn.AsStream())
                     .SendAsync(ctx.Channel);
+            }
             else
-                await new
-                        DiscordMessageBuilder()
-                    .WithContent($"{(int) res.StatusCode} - {res.StatusCode.Humanize(LetterCasing.AllCaps)}\n {(jsn == "{}" ? "Request returned empty response." : Formatter.BlockCode(jsn, "json"))}")
+            {
+                await new DiscordMessageBuilder()
+                    .WithContent($"{(int)res.StatusCode} - {res.StatusCode.Humanize(LetterCasing.AllCaps)}\n {(jsn == "{}" ? "Request returned empty response." : Formatter.BlockCode(jsn, "json"))}")
                     .SendAsync(ctx.Channel);
-
-
+            }
         }
     }
 }
