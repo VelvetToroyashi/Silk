97341225bac73981691c4316984eb7a76e6919dc
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-06-21 05:45:13 -0400

    I'll figure out DTOs eventually

--- src/Silk.Core.Data/DTOs/InfractionDTO.cs
+++ src/Silk.Core.Data/DTOs/InfractionDTO.cs
@@ -1,12 +1,13 @@
-﻿using Silk.Core.Data.Models;
+﻿using System;
+using Silk.Core.Data.Models;
 
 namespace Silk.Core.Data.DTOs
 {
 	public sealed record InfractionDTO
 	{
 		public InfractionDTO(Infraction infraction)
-			: this(infraction.Id, infraction.UserId, infraction.GuildId, infraction.Enforcer, infraction.InfractionType, infraction.Reason, !infraction.HeldAgainstUser) { }
-		public InfractionDTO(int id, ulong userId, ulong guildId, ulong enforcerId, InfractionType type, string reason, bool rescinded)
+			: this(infraction.Id, infraction.UserId, infraction.GuildId, infraction.Enforcer, infraction.InfractionType, infraction.Reason, !infraction.HeldAgainstUser, infraction.Expiration) { }
+		public InfractionDTO(int id, ulong userId, ulong guildId, ulong enforcerId, InfractionType type, string reason, bool rescinded, DateTime? duration = null)
 		{
 			Id = id;
 			UserId = userId;
@@ -15,12 +16,15 @@ namespace Silk.Core.Data.DTOs
 			Reason = reason;
 			Rescinded = rescinded;
 			EnforcerId = enforcerId;
+			Duration = duration;
 		}
 		public int Id { get; init; }
 		public ulong UserId { get; init; }
 		public ulong GuildId { get; init; }
 		public ulong EnforcerId { get; init; }
 		public bool Rescinded { get; init; }
+		
+		public DateTime? Duration { get; init; }
 		public InfractionType Type { get; init; }
 		public string Reason { get; init; }
 	}
