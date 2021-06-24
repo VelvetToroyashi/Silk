c87346d424d050c1e4c8bddde374d9a9507551b7
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-06-23 23:25:32 -0400

    Add documentation

--- src/Silk.Core/Types/InfractionResult.cs
+++ src/Silk.Core/Types/InfractionResult.cs
@@ -1,11 +1,33 @@
 ﻿namespace Silk.Core.Types
 {
+	/// <summary>
+	/// An enum containing semantic values of the result of trying to apply a moderation action to a user.
+	/// </summary>
 	public enum InfractionResult
 	{
+		/// <summary>
+		/// Heirarchy [of roles] prevented an operation from being performed.
+		/// </summary>
 		FailedGuildHeirarchy,
+		/// <summary>
+		/// The current member does not have requisite permissions to perform the action.
+		/// </summary>
+		FailedSelfPermissions,
+		/// <summary>
+		/// The member the infraction applied to left before or during the operation.
+		/// </summary>
 		FailedMemberGuildCache,
+		/// <summary>
+		/// The operation succeeded, but does not notify the member the infraction applies to.
+		/// </summary>
 		SucceededDoesNotNotify,
+		/// <summary>
+		/// The operation succeeded, and the member the infraction was applied to was notified.
+		/// </summary>
 		SucceededWithNotification,
+		/// <summary>
+		/// The operation succeeded, but the member the infraction applies to was not notified.
+		/// </summary>
 		SucceededWithoutNotification,
 	}
 }
