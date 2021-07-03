b8bf73f8c6bc173d2aa7189a6cdb7d430520052f
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-07-03 08:06:12 -0400

    CaseId lookup

--- src/Silk.Core.Data/MediatR/Infractions/GetUserInfractionRequest.cs
+++ src/Silk.Core.Data/MediatR/Infractions/GetUserInfractionRequest.cs
@@ -8,7 +8,7 @@ using Silk.Core.Data.Models;
 
 namespace Silk.Core.Data.MediatR.Infractions
 {
-	public sealed record GetUserInfractionRequest(ulong UserId, ulong GuildId, InfractionType Type) : IRequest<InfractionDTO?>;
+	public sealed record GetUserInfractionRequest(ulong UserId, ulong GuildId, InfractionType Type, int? CaseId = null) : IRequest<InfractionDTO?>;
 
 	public sealed class GetUserInfractionHandler : IRequestHandler<GetUserInfractionRequest, InfractionDTO?>
 	{
@@ -17,13 +17,25 @@ namespace Silk.Core.Data.MediatR.Infractions
 
 		public async Task<InfractionDTO?> Handle(GetUserInfractionRequest request, CancellationToken cancellationToken)
 		{
-			var inf = await _db.Infractions
-				.Where(inf => inf.UserId == request.UserId)
-				.Where(inf => inf.GuildId == request.GuildId)
-				.Where(inf => inf.InfractionType == request.Type)
-				.Where(inf => !inf.HeldAgainstUser)
-				.OrderBy(inf => inf.CaseNumber)
-				.LastOrDefaultAsync(cancellationToken);
+			Infraction? inf;
+			if (request.CaseId is not null)
+			{
+				inf = await _db.Infractions
+					.Where(inf => inf.UserId == request.UserId)
+					.Where(inf => inf.GuildId == request.GuildId)
+					.Where(inf => inf.CaseNumber == request.CaseId)
+					.SingleOrDefaultAsync(cancellationToken);
+			}
+			else
+			{
+				inf = await _db.Infractions
+					.Where(inf => inf.UserId == request.UserId)
+					.Where(inf => inf.GuildId == request.GuildId)
+					.Where(inf => inf.InfractionType == request.Type)
+					.Where(inf => !inf.HeldAgainstUser)
+					.OrderBy(inf => inf.CaseNumber)
+					.LastOrDefaultAsync(cancellationToken);
+			}
 
 			return inf is null ? null : new(inf);
 		}
