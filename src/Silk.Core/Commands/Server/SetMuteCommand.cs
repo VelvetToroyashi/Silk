64a05b23edb178c249897a5577833028a6b5ad39
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-07-31 22:04:32 -0400

    Grammar and clarity + apply code style

--- src/Silk.Core/Commands/Server/SetMuteCommand.cs
+++ src/Silk.Core/Commands/Server/SetMuteCommand.cs
@@ -16,10 +16,7 @@ namespace Silk.Core.Commands.Server
     public class SetMuteCommand : BaseCommandModule
     {
         private readonly IMediator _mediator;
-        public SetMuteCommand(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+        public SetMuteCommand(IMediator mediator) => _mediator = mediator;
 
         [Command]
         [RequireGuild]
@@ -27,11 +24,12 @@ namespace Silk.Core.Commands.Server
         [Description("Set the mute role to use when calling mute command, or auto-mod")]
         public async Task SetMute(CommandContext ctx, DiscordRole role)
         {
-            if ((role.Permissions & Permissions.SendMessages) is not 0) // Does not have permission
+            if (role.Permissions.HasPermission(Permissions.SendMessages)) // Does not have permission
             {
-                await ctx.RespondAsync("Role isn't restrictive!");
+                await ctx.RespondAsync("That role isn't restrictive!");
                 return;
             }
+            
             var builder = new DiscordMessageBuilder();
             builder.WithReply(ctx.Message.Id).WithoutMentions();
 
