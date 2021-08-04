3ca9bc6926c8d4e4a36416f0abe49244fecafb53
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-03 22:18:08 -0400

    [build] test?

--- src/Silk.Core/Commands/Test.cs
+++ src/Silk.Core/Commands/Test.cs
@@ -7,19 +7,21 @@ using DSharpPlus.Entities;
 using DSharpPlus.Menus;
 using DSharpPlus.Menus.Attributes;
 using DSharpPlus.Menus.Entities;
+using Silk.Core.Data.Models;
 using Silk.Core.Types;
+using Silk.Core.Utilities;
 
 namespace Silk.Core.Commands
 {
+	[RequireFlag(UserFlag.Staff)]
 	public class Test : BaseCommandModule
 	{
 		[Command]
-		public async Task ViewConfig(CommandContext ctx)
+		public async Task Config(CommandContext ctx)
 		{
 			var menu = new BaseConfigMenu(ctx.Client);
 			await menu.StartAsync();
 			await ctx.RespondAsync(m => m.WithContent("** **").AddMenu(menu));
-			
 		}
 
 	}
@@ -41,9 +43,6 @@ namespace Silk.Core.Commands
 
 		// For this button to be registered it must have one of the button attributes,
 		// have `ComponentInteractionCreateEventArgs` as first and only parameter and return `Task`
-		[SuccessButton("Test two (this one does nothing)", Row = ButtonPosition.Third)]
-		public Task SuccessAsync(ButtonContext ctx) => Interact(ctx);
-
 		[PrimaryButton("Test one", Row = ButtonPosition.First)]
 		public Task DangerAsync(ButtonContext ctx) => ShowNewMenuAsync(ctx);
 		
@@ -53,15 +52,11 @@ namespace Silk.Core.Commands
 	{
 		public ModConfigMenu(DiscordClient client, Menu previous , TimeSpan? timeout = null) : base(client, previous, timeout) { }
 		
-		[SecondaryButton("Back!")]
+		
+		
+		
+		[SecondaryButton("Back!", Row = ButtonPosition.Fifth, Location = ButtonPosition.Fifth)]
 		public override Task BackAsync(ButtonContext ctx)
 			=> ctx.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage, new DiscordInteractionResponseBuilder().WithContent(ctx.Message.Content).AddMenu(_previous));
-		
-		[SuccessButton("Test?")]
-		public Task Handle(ButtonContext ctx) => ctx.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
-
-		
-		[SuccessButton("Test2?")]
-		public Task Handle2(ButtonContext ctx) => ctx.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
 	}
 }
