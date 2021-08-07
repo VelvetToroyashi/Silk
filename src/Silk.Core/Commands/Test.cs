0721c5d645a4ebbd9e005bdc68f873582ae018e0
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-07 04:41:58 -0400

    Formatting changes

--- src/Silk.Core/Commands/Test.cs
+++ src/Silk.Core/Commands/Test.cs
@@ -9,6 +9,7 @@ using Silk.Core.Data.MediatR.Guilds;
 using Silk.Core.Data.MediatR.Guilds.Config;
 using Silk.Core.Data.Models;
 using Silk.Core.Utilities;
+using Silk.Shared.Constants;
 
 namespace Silk.Core.Commands
 {
@@ -40,7 +41,8 @@ namespace Silk.Core.Commands
 				contentBuilder
 					.Clear()
 					.AppendLine("**General Config:**")
-					.AppendLine($"Greeting option: {config.GreetingOption.Humanize()}")
+					.AppendLine("__Greeting:__ ")
+					.AppendLine($"> Option: {config.GreetingOption.Humanize()}")
 					.AppendLine($"> Greetting channel {(config.GreetingOption is GreetingOption.DoNotGreet ? "N/A" : $"<#{config.GreetingChannel}>")}")
 					.AppendLine($"> Greeting text: {(config.GreetingOption is GreetingOption.DoNotGreet ? "N/A" : $"[See {ctx.Prefix}config view greeting]")}")
 					.AppendLine()
@@ -50,7 +52,13 @@ namespace Silk.Core.Commands
 					.AppendLine($"Max user mentions: {GetCountString(modConfig.MaxUserMentions)}")
 					.AppendLine()
 					.AppendLine($"Mute role: {(modConfig.MuteRoleId is 0 ? "Not set" : $"<@&{modConfig.MuteRoleId}>")}")
-					.AppendLine($"Logging channel: {(modConfig.LoggingChannel is 0 ? "Not set" : $"<#{modConfig.LoggingChannel}>")}");
+					.AppendLine($"Logging channel: {(modConfig.LoggingChannel is 0 ? "Not set" : $"<#{modConfig.LoggingChannel}>")}")
+					.AppendLine()
+					.AppendLine("__Invites:__")
+					.AppendLine($"> Scan invite: <:_:{(modConfig.ScanInvites ? Emojis.ConfirmId : Emojis.DeclineId)}>")
+					.AppendLine($"> Infract on invite: <:_:{(modConfig.WarnOnMatchedInvite ? Emojis.ConfirmId : Emojis.DeclineId)}>")
+					.AppendLine($"> Delete matched invite: <:_:{(modConfig.DeleteMessageOnMatchedInvite ? Emojis.ConfirmId : Emojis.DeclineId)}>")
+					.AppendLine($"> Allowed invites: {(modConfig.AllowedInvites.Count is 0 ? "None" : $"{modConfig.AllowedInvites.Count} allowed invites [See {ctx.Prefix}config view invites]")}");
 				
 				embed
 					.WithTitle($"Configuration for {ctx.Guild.Name}:")
