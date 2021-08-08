0c082da2a51c1256b74f6a4cf45b0111c2a9fa0a
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-08 03:08:15 -0400

    Add more views to Test.cs

--- src/Silk.Core/Commands/Test.cs
+++ src/Silk.Core/Commands/Test.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -27,14 +28,14 @@ namespace Silk.Core.Commands
 			public ViewConfigModule(IMediator mediator) => _mediator = mediator;
 
 			private string GetCountString(int count) => count is 0 ? "Not set/enabled" : count.ToString();
-			
-			
+
+
 			[GroupCommand]
 			public async Task View(CommandContext ctx)
 			{
 				var config = await _mediator.Send(new GetGuildConfigRequest(ctx.Guild.Id));
 				var modConfig = await _mediator.Send(new GetGuildModConfigRequest(ctx.Guild.Id));
-				
+
 				var embed = new DiscordEmbedBuilder();
 				var contentBuilder = new StringBuilder();
 
@@ -58,14 +59,15 @@ namespace Silk.Core.Commands
 					.AppendLine($"> Scan invite: <:_:{(modConfig.ScanInvites ? Emojis.ConfirmId : Emojis.DeclineId)}>")
 					.AppendLine($"> Infract on invite: <:_:{(modConfig.WarnOnMatchedInvite ? Emojis.ConfirmId : Emojis.DeclineId)}>")
 					.AppendLine($"> Delete matched invite: <:_:{(modConfig.DeleteMessageOnMatchedInvite ? Emojis.ConfirmId : Emojis.DeclineId)}>")
+					.AppendLine($@"> Use agressive invite matching (`disc((ord)?(((app)?\.com\/invite)|(\.gg)))\/([A-z0-9-_]{{2,}})`): <:_:{(modConfig.UseAggressiveRegex ? Emojis.ConfirmId : Emojis.DeclineId)}>>>")
 					.AppendLine($"> Allowed invites: {(modConfig.AllowedInvites.Count is 0 ? "None" : $"{modConfig.AllowedInvites.Count} allowed invites [See {ctx.Prefix}config view invites]")}")
-					.AppendLine($@"> Use agressive invite matching (`disc((ord)?(((app)?\\.com\/invite)|(\\.gg)))\\/([A-z0-9-_]{{2,}})`): <:_:{(modConfig.UseAggressiveRegex ? Emojis.ConfirmId : Emojis.DeclineId)}")
 					.AppendLine()
 					.AppendLine("__Infractions:__")
 					.AppendLine($"> Infraction steps: {(modConfig.InfractionSteps.Count is var dictCount and not 0 ? $"{dictCount} steps [See {ctx.Prefix}config view infractions]" : "Not configured")}")
-					.AppendLine($"> Infraction steps (named): {(modConfig.NamedInfractionSteps.Count is var infNameCount and not 0 ? $"{infNameCount} steps [See {ctx.Prefix}config view infractions]" : "Not configured")}")
-					.AppendLine($"> Auto-escalate automod infractions: <:_:{(modConfig.AutoEscalateInfractions ? Emojis.ConfirmId : Emojis.DeclineId)}>")
-					.AppendLine($"> ");
+					.AppendLine($"> Infraction steps (named): {((modConfig.NamedInfractionSteps?.Count ?? 0) is var infNameCount and not 0 ? $"{infNameCount} steps [See {ctx.Prefix}config view infractions]" : "Not configured")}")
+					.AppendLine($"> Auto-escalate automod infractions: <:_:{(modConfig.AutoEscalateInfractions ? Emojis.ConfirmId : Emojis.DeclineId)}>");
+
+
 				embed
 					.WithTitle($"Configuration for {ctx.Guild.Name}:")
 					.WithColor(DiscordColor.Azure)
@@ -73,6 +75,49 @@ namespace Silk.Core.Commands
 
 				await ctx.RespondAsync(embed);
 			}
+
+			[Command]
+			public async Task Greeting(CommandContext ctx)
+			{
+				var contentBuilder = new StringBuilder();
+				var config = await _mediator.Send(new GetGuildConfigRequest(ctx.Guild.Id));
+
+				contentBuilder
+					.Clear()
+					.AppendLine("__**Greeting option:**__")
+					.AppendLine($"> Option: {config.GreetingOption.Humanize()}")
+					.AppendLine($"> Greetting channel {(config.GreetingOption is GreetingOption.DoNotGreet ? "N/A" : $"<#{config.GreetingChannel}>")}")
+					.AppendLine($"> Greeting text: {(config.GreetingOption is GreetingOption.DoNotGreet ? "N/A" : $"\n\n{config.GreetingText}")}")
+					.AppendLine($"> Greeting role: {(config.GreetingOption is GreetingOption.GreetOnRole && config.VerificationRole is var role and not 0 ? $"<@&{role}>" : "N/A")}");
+
+				var explanation = config.GreetingOption switch
+				{
+					GreetingOption.DoNotGreet => "I will not greet members at all.",
+					GreetingOption.GreetOnJoin => "I will greet members as soon as they join",
+					GreetingOption.GreetOnRole => "I will greet members when they're given a specific role",
+					GreetingOption.GreetOnScreening => "I will greet members when they pass membership screening. Only applicable to community servers.",
+					_ => throw new ArgumentOutOfRangeException()
+				};
+
+				contentBuilder
+					.AppendLine()
+					.AppendLine("**Greeting option explanation:**")
+					.AppendLine(explanation);
+
+				var embed = new DiscordEmbedBuilder()
+					.WithColor(DiscordColor.Azure)
+					.WithTitle($"Config for {ctx.Guild.Name}")
+					.WithDescription(contentBuilder.ToString());
+
+				await ctx.RespondAsync(embed);
+			}
+
+			[Command]
+			public async Task Invites(CommandContext ctx)
+			{
+				var config = await _mediator.Send(new GetGuildModConfigRequest(ctx.Guild.Id));
+			}
+			
 		}
 	}
 }
