7d974c25cffdd567c25529f9a643b463d53f0d67
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-10 03:37:39 -0400

    AutoMod strings

---
+++ src/Silk.Shared/Constants/AutoModConstants.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Silk.Shared.Constants
+{
+	/// <summary>
+	/// <para>A class that defines all string keys for AutoMod actions.</para>
+	/// 
+	/// <para>If a key is not present in a config dictionary, the config's configured InfractionStep is used. </para>
+	/// </summary>
+	public static class AutoModConstants
+	{
+		/// <summary>
+		/// The maximum amount of unique user mentions a message can contain.
+		/// </summary>
+		public const string MaxUserMentions = "max_user_mentions";
+		
+		/// <summary>
+		/// The maximum amount of unique role mentions a message can contain.
+		/// </summary>
+		public const string MaxRoleMentions = "max_role_mentions";
+		
+		//TODO: More automod strings
+	}
+}
