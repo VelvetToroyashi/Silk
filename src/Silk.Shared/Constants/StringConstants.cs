e393878df9790c8fe1a6beecaf73679655c4570b
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-10 02:16:02 -0400

    Bump version to 2.2

--- src/Silk.Shared/Constants/StringConstants.cs
+++ src/Silk.Shared/Constants/StringConstants.cs
@@ -3,7 +3,7 @@
     public static class StringConstants
     {
         public const string DefaultCommandPrefix = "s!";
-        public const string Version = "2.1.2-alpha";
+        public const string Version = "2.2";
         public const string LogFormat = "[{Timestamp:h:mm:ss ff tt}] [{Level:u3}] [{SourceContext}] {Message:lj} {Exception:j}{NewLine}";
         public const string HttpClientName = "Silk";
         public const string AutoModMessagePrefix = "[AUTOMOD]";
