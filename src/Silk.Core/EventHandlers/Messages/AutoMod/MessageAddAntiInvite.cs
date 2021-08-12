1a26caf175337f8314af4a1e0a972118dd41d986
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-12 02:41:10 -0400

    [build] remove logging

--- src/Silk.Core/EventHandlers/Messages/AutoMod/MessageAddAntiInvite.cs
+++ src/Silk.Core/EventHandlers/Messages/AutoMod/MessageAddAntiInvite.cs
@@ -1,7 +1,6 @@
 ﻿using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.EventArgs;
-using Serilog;
 using Silk.Core.Data.Models;
 using Silk.Core.Services.Data;
 
@@ -24,27 +23,15 @@ namespace Silk.Core.EventHandlers.Messages.AutoMod
 			{
 				GuildModConfig? config = await _config.GetModConfigAsync(args.Guild.Id);
 				
-				if (config is null) Log.Warning("Null config");
-				
 				bool hasInvite = _inviteHelper.CheckForInvite(args.Message, config, out string invite);
+				
 				if (!hasInvite)
-				{
-					Log.Warning("No invite");
 					return;
-				}
 				
 				bool isBlacklisted = await _inviteHelper.IsBlacklistedInvite(args.Message, config, invite);
 
 				if (isBlacklisted)
-				{
-					Log.Warning("Blacklisted invite");
 					await _inviteHelper.TryAddInviteInfractionAsync(args.Message, config);
-				}
-				else
-				{
-					Log.Warning("Non-blacklisted invite");
-				}
-				
 			}
 		}
 	}
