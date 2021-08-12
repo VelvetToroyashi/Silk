8e67a25961ee5ad9fb7d8320f0db97965a465d47
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-12 02:13:27 -0400

    [build] help

--- src/Silk.Core/EventHandlers/Messages/AutoMod/MessageAddAntiInvite.cs
+++ src/Silk.Core/EventHandlers/Messages/AutoMod/MessageAddAntiInvite.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.EventArgs;
+using Serilog;
 using Silk.Core.Data.Models;
 using Silk.Core.Services.Data;
 
@@ -22,14 +23,28 @@ namespace Silk.Core.EventHandlers.Messages.AutoMod
 			if (!args.Channel.IsPrivate && args.Author != client.CurrentUser)
 			{
 				GuildModConfig? config = await _config.GetModConfigAsync(args.Guild.Id);
-
+				
+				if (config is null) Log.Warning("Null config");
+				
 				bool hasInvite = _inviteHelper.CheckForInvite(args.Message, config, out string invite);
-				if (!hasInvite) return;
+				if (!hasInvite)
+				{
+					Log.Warning("No invite");
+					return;
+				}
 				
 				bool isBlacklisted = await _inviteHelper.IsBlacklistedInvite(args.Message, config, invite);
 
 				if (isBlacklisted)
+				{
+					Log.Warning("Blacklisted invite");
 					await _inviteHelper.TryAddInviteInfractionAsync(args.Message, config);
+				}
+				else
+				{
+					Log.Warning("Non-blacklisted invite");
+				}
+				
 			}
 		}
 	}
