44c8f48d9ec97ffb9742eb60d3e0fb30ef550c18
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-14 02:24:04 -0400

    No idea what I did but I did it

---
+++ plugins/TestPlugin/PluginManifest.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using YumeChan.PluginBase;
+
+namespace TestPlugin
+{
+	public class PluginManifest : Plugin
+	{
+		private readonly ILogger<PluginManifest> _logger;
+		public override string PluginDisplayName => "Silk! Test Plugin";
+		public override bool PluginStealth => false;
+
+		public PluginManifest(ILogger<PluginManifest> logger)
+		{
+			_logger = logger;
+		}
+
+		public override async Task LoadPlugin()
+		{
+			await base.LoadPlugin();
+			_logger.LogInformation("Loaded successfully!");
+		}
+	}
+}
