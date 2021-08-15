c739f62928ecd46e25093ccf02181f330c0aa7f6
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-15 18:02:13 -0400

    Update method of getting version name

--- plugins/RoleMenuPlugin/Plugin/RoleMenuPlugin.cs
+++ plugins/RoleMenuPlugin/Plugin/RoleMenuPlugin.cs
@@ -19,12 +19,14 @@ namespace RoleMenuPlugin
 			_roleMenu = roleMenu;
 			_client = client;
 			_db = db;
+
+			Version = this.GetType().Assembly.GetName().Version!.ToString(3);
 		}
 
 		public override async Task LoadAsync()
 		{
 			await base.LoadAsync(); // Simply sets Loaded to true //
-
+		
 			await _db.Database.MigrateAsync();
 			
 			_client.ComponentInteractionCreated += _roleMenu.Handle;
