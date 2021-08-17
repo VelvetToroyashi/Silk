8b3dfcecc1a0137f3439197c96bce80fc548709d
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-17 09:46:15 -0400

    PluginManifest record

---
+++ plugins/PluginLoader.Unity/PluginManifest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using YumeChan.PluginBase;
+
+namespace PluginLoader.Unity
+{
+	public record PluginManifest
+	{
+		public Plugin Plugin { get; init; }
+		public Assembly Assembly { get; init; }
+		public FileInfo PluginInfo { get; init; }
+		public AppDomain AssociatedDomain { get; init; }
+
+
+		public static implicit operator Plugin(PluginManifest manifest) => manifest.Plugin;
+	}
+}
