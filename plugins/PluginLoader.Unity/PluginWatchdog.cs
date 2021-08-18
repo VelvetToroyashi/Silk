162f87f11062a6efef05a7fe9f500b29996d23d7
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-18 02:15:05 -0400

    Add extension method class and plugin watchdog

---
+++ plugins/PluginLoader.Unity/PluginWatchdog.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Microsoft.Extensions.Logging;
+
+namespace PluginLoader.Unity
+{
+	public sealed class PluginWatchdog
+	{
+		private readonly FileSystemWatcher _fileWatcher = new("./plugins", "*Plugin.dll");
+		
+		private readonly ILogger<PluginWatchdog> _logger;
+		private readonly PluginLoader _loader;
+		
+		public PluginWatchdog(ILogger<PluginWatchdog> logger, PluginLoader loader)
+		{
+			_logger = logger;
+			_loader = loader;
+
+			_fileWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+			_fileWatcher.EnableRaisingEvents = true;
+			
+			_fileWatcher.Created += LoadPlugin;
+			_fileWatcher.Changed += ReloadPlugin;
+			_fileWatcher.Deleted += UnloadPlugin;
+		}
+		
+		private void UnloadPlugin(object sender, FileSystemEventArgs e) { }
+		private async void ReloadPlugin(object sender, FileSystemEventArgs e)
+		{
+			await _loader.LoadNewPluginAsync(new FileInfo(e.FullPath));
+		}
+
+		private async void LoadPlugin(object sender, FileSystemEventArgs e)
+		{
+			await _loader.LoadNewPluginAsync(new FileInfo(e.FullPath));
+		}
+
+
+	}
+}
