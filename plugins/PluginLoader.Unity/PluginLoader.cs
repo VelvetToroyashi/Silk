b6631d6c7911f3f0f00aa25dd0cdcfa84c880b42
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-22 04:25:15 -0400

    no more plugins

--- plugins/PluginLoader.Unity/PluginLoader.cs
+++ plugins/PluginLoader.Unity/PluginLoader.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.Loader;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
-using Unity;
-using Unity.Microsoft.DependencyInjection;
-using YumeChan.PluginBase;
 
 namespace PluginLoader.Unity
 { 
@@ -37,7 +27,7 @@ namespace PluginLoader.Unity
 	/// <summary>
 	/// A helper class that loads and instantiates plugins from assemblies located in the plugins folder.
 	/// </summary>
-	public sealed class PluginLoader : IEnumerable<Plugin>
+	public sealed class PluginLoader
 	{
 		private readonly ILogger<PluginLoader> _logger;
 		public IReadOnlyList<PluginManifest> Plugins => _plugins;
@@ -45,139 +35,7 @@ namespace PluginLoader.Unity
 		
 		public PluginLoader(ILogger<PluginLoader> logger) => _logger = logger;
 
-		/// <summary>
-		/// Loads plugin manifests from Disk. Plugins must be placed in the plugins folder relative to the core binary.
-		/// </summary>
-		internal async Task LoadPluginFilesAsync()
-		{
-			Directory.CreateDirectory("./plugins");
-			var pluginFiles = Directory.GetFiles("./plugins", "*Plugin.dll")
-				.Where(f => _plugins.All(m => !string.Equals(f, m.PluginInfo.Name)));
-
-			foreach (var plugin in pluginFiles)
-			{
-				var fileInfo = new FileInfo(plugin);
-				var loadContext = new AssemblyLoadContext(fileInfo.Name, true);
-				var asmStream = File.Open(fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete);
-
-				var asm = loadContext.LoadFromStream(asmStream);
-				asmStream.Close();
-				
-				var manifest = new PluginManifest()
-				{
-					Assembly = asm,
-					PluginInfo = fileInfo,
-					LoadContext = loadContext
-				};
-
-				_plugins.Add(manifest);
-			}
-		}
 		
-		/// <summary>
-		/// Loads a new plugin, unloading the old plugin, if it exists.
-		/// </summary>
-		/// <param name="info">The info of the plugin file.</param>
-		internal async Task LoadNewPluginManifestAsync(FileInfo info)
-		{
-			if (_plugins.Select(p => p.PluginInfo).SingleOrDefault(f => f.Name == info.Name) is {} fi)
-			{
-				if (info.LastWriteTime <= fi.CreationTime)
-					return; // ??? Not a new file. //
-
-				await UnloadPlugin(info);
-			}
-			
-			var loadContext = new AssemblyLoadContext(info.Name, true);
-			var asmStream = File.Open(info.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete);
-
-			var asm = loadContext.LoadFromStream(asmStream);
-			asmStream.Close();
-			
-			var manifest = new PluginManifest()
-			{
-				Assembly = asm,
-				PluginInfo = info,
-				LoadContext = loadContext
-			};
-
-			_plugins.Add(manifest);
-			
-			_logger.LogDebug("Hot-loaded plugin file {PluginFile}", info.Name);
-		}
-
-
-		/// <summary>
-		/// Unloads a plugin from memory.
-		/// </summary>
-		/// <param name="info"></param>
-		internal async Task UnloadPlugin(FileInfo info)
-		{
-			var plugin = _plugins.SingleOrDefault(p => p.PluginInfo.Name == info.Name);
-
-			if (plugin is null)
-				return;
-
-			try
-			{
-				await plugin.Plugin.UnloadAsync()!;
-			}
-			catch (Exception e)
-			{
-				_logger.LogWarning(e, "Unloading a plugin throw an exception. Plugin: {Plugin}, Exception:", plugin.Plugin!.DisplayName);	
-			}
-			
-			plugin.LoadContext.Unload();
-			_plugins.Remove(plugin);
-			_logger.LogDebug("Successfully unloaded {Plugin}.", plugin.PluginInfo.Name);
-		}
-		
-		/// <summary>
-		/// Instantiates services for the plugin assemblies. This should be called AFTER calling <see cref="LoadPluginFilesAsync"/>.
-		/// </summary>
-		/// <param name="container">The service container to add services to.</param>
-		/// <param name="manifests">Optional: Specify the plugin manifests to load</param>
-		internal PluginLoader InstantiatePluginServices(IUnityContainer container, IEnumerable<PluginManifest> manifests = null)
-		{
-			foreach (var plugin in manifests ?? _plugins)
-				foreach (var t in plugin.Assembly.ExportedTypes.Where(t => t.IsSubclassOf(typeof(DependencyInjectionHandler))))
-				{
-					var services = (container.Resolve(t) as DependencyInjectionHandler)!.ConfigureServices(new ServiceCollection());
-					
-					if (services is not null)
-						container.AddServices(services);
-				}
-
-			return this;
-		}
-
-		/// <summary>
-		/// Instantiates plugins, but does not start them.
-		/// </summary>
-		/// <param name="container">The service container to add plugins to.</param>
-		internal PluginLoader AddPlugins(IUnityContainer container)
-		{
-			foreach (var asm in _plugins.Where(p => p.Plugin is null))
-				foreach (var t in asm.Assembly.ExportedTypes.Where(t => t.IsSubclassOf(typeof(Plugin))))
-				{
-					try
-					{
-						var plugin = container.Resolve(t) as Plugin;
-						container.RegisterInstance(typeof(Plugin), plugin);
-						asm.Plugin = plugin;
-					}
-					catch (ResolutionFailedException resex)
-					{
-						_logger.LogError("{Plugin} v{Version} defined as {Assembly} failed to register its services. {Type} was not provided in the container", 
-							t.Name, t.Assembly.GetName().Version!.ToString(3), t.Assembly.Location, resex.TypeRequested);
-					}
-				}
-				
-			return this;
-		}
-		
-		public IEnumerator<Plugin> GetEnumerator() => _plugins.Select(p => p.Plugin).GetEnumerator();
-		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 		
 	}
 }
