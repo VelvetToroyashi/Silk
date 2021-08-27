751605b6ff1ea9b92472b0f437bcc3ee5987af66
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-26 20:42:00 -0400

    Add Validation behavior and extension method to register it

---
+++ api/Silk.Api.Domain/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Silk.Api.Domain.Behaviors;
+using Silk.Api.Domain.Feature.Infractions;
+
+namespace Silk.Api.Domain
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddValidators(this IServiceCollection services)
+		{
+			services.AddTransient<IValidator<AddInfraction.Request>, AddInfraction.Validator>();
+			services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+			
+			return services;
+		}
+	}
+}
