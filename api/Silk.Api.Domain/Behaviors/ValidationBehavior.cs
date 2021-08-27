c0608f9940ad9dfb95439ce25f5ecc1ef54bca85
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-26 20:54:52 -0400

    Seal ValidationBehavior.cs and apply code style

--- api/Silk.Api.Domain/Behaviors/ValidationBehavior.cs
+++ api/Silk.Api.Domain/Behaviors/ValidationBehavior.cs
@@ -7,11 +7,11 @@ using MediatR;
 
 namespace Silk.Api.Domain.Behaviors
 {
-	public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+	public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 	{
 		private readonly IEnumerable<IValidator<TRequest>> _validators;
 		public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators;
-		
+
 		public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
 		{
 			if (_validators.Any())
@@ -22,7 +22,7 @@ namespace Silk.Api.Domain.Behaviors
 
 				var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
 
-				if (failures.Count != 0)
+				if (failures.Count is not 0)
 					throw new ValidationException(failures);
 			}
 			return await next();
