b8ac0d26ec96bbee5da5d66860d0c21e9afa1f43
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-26 20:15:16 -0400

    Fix ApiContext ctor

--- api/Silk.Api.Data/ApiContext.cs
+++ api/Silk.Api.Data/ApiContext.cs
@@ -5,6 +5,7 @@ namespace Silk.Api.Data
 {
 	public class ApiContext : DbContext
 	{
+		public ApiContext(DbContextOptions options) : base(options) { }
 		public DbSet<Infraction> Infractions { get; set; }
 	}
 }
