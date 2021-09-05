45537f6678bd6a37752c80353a463f2b339a4f36
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-09-04 22:38:23 -0400

    Register services

--- plugins/MusicPlugin/Plugin/DependencyHandler.cs
+++ plugins/MusicPlugin/Plugin/DependencyHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MusicPlugin.Services;
 using YumeChan.PluginBase;
 
 namespace MusicPlugin
@@ -7,7 +8,9 @@ namespace MusicPlugin
 	{
 		public override IServiceCollection ConfigureServices(IServiceCollection services)
 		{
-			return services;
+			return services
+				.AddSingleton<MusicApiService>()
+				.AddSingleton<MusicWatchDogService>();
 		}
 	}
 }
