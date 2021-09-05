8188a81d890643a46e7401a95b2a152e04c33ade
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-09-04 22:38:42 -0400

    Add MusicQueueService; will work on later

---
+++ plugins/MusicPlugin/Services/MusicQueueService.cs
@@ -0,0 +1,10 @@
+﻿using DSharpPlus.Entities;
+
+namespace MusicPlugin.Services
+{
+	public sealed class MusicQueueService
+	{
+		public bool PlayingInGuild(ulong guildId) => false;
+		public DiscordChannel GetBoundChannelForGuild(ulong guildId) => null;
+	}
+}
