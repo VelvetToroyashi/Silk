3289e1d98fa1b3bfe2892abcbf1216191bb5cbf0
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-11-30 09:51:56 -0500

    Add extension class for services and remora

--- src/Silk.Core/Program.cs
+++ src/Silk.Core/Program.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -9,23 +7,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Remora.Commands.Extensions;
-using Remora.Discord.Caching.Extensions;
-using Remora.Discord.Commands.Extensions;
-using Remora.Discord.Hosting.Extensions;
 using Serilog;
-using Serilog.Events;
-using Serilog.Templates;
 using Silk.Core.Data;
 using Silk.Core.Services.Data;
 using Silk.Core.Services.Interfaces;
-using Silk.Core.Utilities;
-using Silk.Core.Utilities.HelpFormatter;
-using Silk.Extensions;
-using Silk.Extensions.Remora;
-using Silk.Shared;
 using Silk.Shared.Configuration;
-using Silk.Shared.Constants;
 
 namespace Silk.Core
 {
@@ -76,70 +62,23 @@ namespace Silk.Core
 					AddSilkConfigurationOptions(services, context.Configuration);
 					AddDatabases(services, silkConfig.Persistence);
 
-					services.AddLogging(_ => AddLogging(context));
-
-					var asm = Assembly.GetExecutingAssembly();
+					services.AddSilkLogging(context);
 
 					services.AddSingleton<IPrefixCacheService, PrefixCacheService>();
+					services.AddSingleton<GuildCacherService>();
 					//services.AddScoped<SilkCommandResponder>(); // So Remora's default responder can be overridden. I'll remove this when my PR is merged. //
+					services.AddRemoraServices();
 
-					services
-						//.AddInteractivity()
-						.AddResponders(asm);
-
-					services
-						.AddScoped<CommandHelpViewer>()
-						.AddScoped<IHelpFormatter, HelpFormatter>();
-
-					services
-						//.AddPostExecutionEvent<FailedCommandResponder>()
-						.AddCommands(asm) // Register types
-						.AddCommands(); // Register commands
-					//.Replace(ServiceDescriptor.Scoped<CommandResponder>(s => s.GetRequiredService<SilkCommandResponder>()));
-
-					services
-						.AddDiscordCommands()
-						.AddDiscordCaching();
-
-					services.AddMemoryCache();
 
 					services.AddMediatR(typeof(Program));
 					services.AddMediatR(typeof(GuildContext));
 				})
-				.AddDiscordService(s =>
-				{
-					SilkConfigurationOptions? config = s.Get<IConfiguration>()!.GetSilkConfigurationOptionsFromSection();
-
-					return config.Discord.BotToken;
-				});
+				.AddRemoraHosting();
 
 			return builder;
 		}
 
-		private static void AddLogging(HostBuilderContext host)
-		{
-			LoggerConfiguration logger = new LoggerConfiguration()
-				.Enrich.FromLogContext()
-				.WriteTo.Console(new ExpressionTemplate(StringConstants.LogFormat, theme: SilkLogTheme.TemplateTheme))
-				.WriteTo.File("./logs/silkLog.log", LogEventLevel.Verbose, StringConstants.FileLogFormat, retainedFileCountLimit: null, rollingInterval: RollingInterval.Day, flushToDiskInterval: TimeSpan.FromMinutes(1))
-				.MinimumLevel.Override("Microsoft", LogEventLevel.Error)
-				.MinimumLevel.Override("DSharpPlus", LogEventLevel.Warning)
-				.MinimumLevel.Override("System.Net", LogEventLevel.Fatal);
 
-			string? configOptions = host.Configuration["Logging"];
-			Log.Logger = configOptions switch
-			{
-				"All" => logger.MinimumLevel.Verbose().CreateLogger(),
-				"Info" => logger.MinimumLevel.Information().CreateLogger(),
-				"Debug" => logger.MinimumLevel.Debug().CreateLogger(),
-				"Warning" => logger.MinimumLevel.Warning().CreateLogger(),
-				"Error" => logger.MinimumLevel.Error().CreateLogger(),
-				"Panic" => logger.MinimumLevel.Fatal().CreateLogger(),
-				_ => logger.MinimumLevel.Verbose().CreateLogger()
-			};
-
-			Log.Logger.ForContext(typeof(Program)).Information("Logging Initialized!");
-		}
 
 		private static void AddSilkConfigurationOptions(IServiceCollection services, IConfiguration configuration)
 		{
