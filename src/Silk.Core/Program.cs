fce1f94f0aa196e80c0b3c262d499ae7c677038e
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-04 17:55:17 -0500

    Add back auto-migrator & requisite services

--- src/Silk.Core/Program.cs
+++ src/Silk.Core/Program.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +19,7 @@ using Silk.Core.Data;
 using Silk.Core.Responders;
 using Silk.Core.Services.Data;
 using Silk.Core.Services.Interfaces;
+using Silk.Core.Services.Server;
 using Silk.Shared.Configuration;
 
 namespace Silk.Core
@@ -24,20 +28,24 @@ namespace Silk.Core
     {
         public static async Task Main()
         {
-            IHostBuilder? host = Host
+            IHostBuilder? hostBuilder = Host
                                 .CreateDefaultBuilder()
                                 .UseConsoleLifetime();
 
-            host.ConfigureAppConfiguration(configuration =>
+            hostBuilder.ConfigureAppConfiguration(configuration =>
             {
                 configuration.SetBasePath(Directory.GetCurrentDirectory());
                 configuration.AddJsonFile("appSettings.json", true, false);
                 configuration.AddUserSecrets("VelvetThePanda-Silk", false);
             });
 
-            ConfigureServices(host);
+            ConfigureServices(hostBuilder);
+            
+            var host = hostBuilder.Build();
 
-            await host.Build().RunAsync();
+            await EnsureDatabaseCreatedAndApplyMigrations(host);
+            
+            await host.RunAsync();
         }
 
         [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "EFCore CLI tools rely on reflection.")]
@@ -49,47 +57,72 @@ namespace Silk.Core
             builder.ConfigureServices((context, container) =>
             {
                 SilkConfigurationOptions? silkConfig = context.Configuration.GetSilkConfigurationOptionsFromSection();
+                
                 AddDatabases(container, silkConfig.Persistence);
             });
 
             return builder;
         }
+        
+        private static async Task EnsureDatabaseCreatedAndApplyMigrations(IHost builtBuilder)
+        {
+            try
+            {
+                using IServiceScope? serviceScope = builtBuilder.Services?.CreateScope();
+                if (serviceScope is not null)
+                {
+                    await using GuildContext? dbContext = serviceScope.ServiceProvider
+                                                                      .GetRequiredService<IDbContextFactory<GuildContext>>()
+                                                                      .CreateDbContext();
+
+                    IEnumerable<string>? pendingMigrations = await dbContext.Database.GetPendingMigrationsAsync();
+
+                    if (pendingMigrations.Any())
+                        await dbContext.Database.MigrateAsync();
+                }
+            }
+            catch (Exception) { }
+        }
 
         private static IHostBuilder ConfigureServices(IHostBuilder builder)
         {
-            builder
-               .ConfigureLogging(l => l.ClearProviders().AddSerilog())
-               .ConfigureServices((context, services) =>
-                {
-                    // There's a more elegant way to do this, but I'm lazy and this works.
-                    SilkConfigurationOptions? silkConfig = context.Configuration.GetSilkConfigurationOptionsFromSection();
+        builder
+           .ConfigureLogging(l => l.ClearProviders().AddSerilog())
+           .ConfigureServices((context, services) =>
+            {
+                // There's a more elegant way to do this, but I'm lazy and this works.
+                SilkConfigurationOptions? silkConfig = context.Configuration.GetSilkConfigurationOptionsFromSection();
 
-                    AddSilkConfigurationOptions(services, context.Configuration);
-                    AddDatabases(services, silkConfig.Persistence);
+                AddSilkConfigurationOptions(services, context.Configuration);
+                AddDatabases(services, silkConfig.Persistence);
 
-                    services.AddSilkLogging(context);
+                services.AddSilkLogging(context);
 
-                    services.AddResponder<EarlyCacheSnapshotResponder>(ResponderGroup.Early);
-                    services.AddResponder<LateCacheSnapshotRespodner>(ResponderGroup.Late);
-                    
-                    services.AddCondition<RequireNSFWCondition>();
-                    
-                    services.AddSingleton<IPrefixCacheService, PrefixCacheService>();
-                    services.AddSingleton<GuildCacherService>();
-                    //services.AddScoped<SilkCommandResponder>(); // So Remora's default responder can be overridden. I'll remove this when my PR is merged. //
-                    services.AddRemoraServices();
+                services.AddResponder<EarlyCacheSnapshotResponder>(ResponderGroup.Early);
+                services.AddResponder<LateCacheSnapshotRespodner>(ResponderGroup.Late);
+                
+                services.AddCondition<RequireNSFWCondition>();
+                
+                
+                services.AddSingleton<IPrefixCacheService, PrefixCacheService>();
+                services.AddSingleton<ICacheUpdaterService, CacheUpdaterService>();
+                services.AddSingleton<GuildConfigCacheService>();
+                services.AddSingleton<GuildCacherService>();
+                services.AddSingleton<GuildGreetingService>();
+                
+                //services.AddScoped<SilkCommandResponder>(); // So Remora's default responder can be overridden. I'll remove this when my PR is merged. //
+                services.AddRemoraServices();
 
 
-                    services.AddMediatR(typeof(Program));
-                    services.AddMediatR(typeof(GuildContext));
-                })
-               .AddRemoraHosting();
+                services.AddMediatR(typeof(Program));
+                services.AddMediatR(typeof(GuildContext));
+            })
+           .AddRemoraHosting();
 
-            return builder;
+        return builder;
         }
 
-
-
+        
         private static void AddSilkConfigurationOptions(IServiceCollection services, IConfiguration configuration)
         {
             // Add and Bind IOptions configuration for appSettings.json and UserSecrets configuration structure
@@ -112,20 +145,18 @@ namespace Silk.Core
             services.AddDbContext<GuildContext>(Builder, ServiceLifetime.Transient);
             services.AddDbContextFactory<GuildContext>(Builder, ServiceLifetime.Transient);
             //services.TryAdd(new ServiceDescriptor(typeof(GuildContext), p => p.GetRequiredService<IDbContextFactory<GuildContext>>().CreateDbContext(), ServiceLifetime.Transient));
+            }
         }
-    }
 
     //Todo: Move this class maybe? 
     public static class IConfigurationExtensions
     {
-	    /// <summary>
-	    ///     An extension method to get a <see cref="SilkConfigurationOptions" /> instance from the Configuration by Section Key
-	    /// </summary>
-	    /// <param name="config">the configuration</param>
-	    /// <returns>an instance of the SilkConfigurationOptions class, or null if not found</returns>
-	    public static SilkConfigurationOptions GetSilkConfigurationOptionsFromSection(this IConfiguration config)
-        {
-            return config.GetSection(SilkConfigurationOptions.SectionKey).Get<SilkConfigurationOptions>();
-        }
+        /// <summary>
+        ///     An extension method to get a <see cref="SilkConfigurationOptions" /> instance from the Configuration by Section Key
+        /// </summary>
+        /// <param name="config">the configuration</param>
+        /// <returns>an instance of the SilkConfigurationOptions class, or null if not found</returns>
+        public static SilkConfigurationOptions GetSilkConfigurationOptionsFromSection(this IConfiguration config) 
+            => config.GetSection(SilkConfigurationOptions.SectionKey).Get<SilkConfigurationOptions>();
     }
 }
