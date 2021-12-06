6c1005e34c82d63b98973da8ff50b2a527ca3f58
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-05 23:34:06 -0500

    Generate migration for deprecated reminder property

--- src/Silk.Core.Data/Migrations/20211206043338_DeprecateRecurringReminders.Designer.cs
+++ src/Silk.Core.Data/Migrations/20211206043338_DeprecateRecurringReminders.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Silk.Core.Data;
@@ -10,9 +11,10 @@ using Silk.Core.Data;
 namespace Silk.Core.Data.Migrations
 {
     [DbContext(typeof(GuildContext))]
-    partial class SilkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211206043338_DeprecateRecurringReminders")]
+    partial class DeprecateRecurringReminders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
