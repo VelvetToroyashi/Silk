3524741646e78daf1fb731ebfcad1a6f8840d157
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-07 20:55:50 -0500

    Remove target and difference from HierarchyError

--- src/Silk.Core/Infrastructure/Errors/HierarchyError.cs
+++ src/Silk.Core/Infrastructure/Errors/HierarchyError.cs
@@ -1,4 +1,3 @@
-using Remora.Rest.Core;
 using Remora.Results;
 
 namespace Silk.Core.Errors
@@ -7,7 +6,5 @@ namespace Silk.Core.Errors
     /// Represents an error that occurs when hiearchy prevents an action from being performed.
     /// </summary>
     /// <param name="Message">The message of the error.</param>
-    /// <param name="Target">The target that cannot be acted upon.</param>
-    /// <param name="Difference">The difference in hierarchy. This is optional.</param>
-    public record HierarchyError(string Message, Snowflake Target, int? Difference = null) : IResultError;
+    public record HierarchyError(string Message) : IResultError;
 }
