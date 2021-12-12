9bc3956d8518a92e55df169bb066b967d34d5ed7
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-12 00:59:33 -0500

    Bump core to 3.0.0-alpha-1

--- src/Silk.Shared/Constants/StringConstants.cs
+++ src/Silk.Shared/Constants/StringConstants.cs
@@ -4,7 +4,7 @@ public static class StringConstants
 {
 
     public const string DefaultCommandPrefix = "s!";
-    public const string Version              = "2.2.15";
+    public const string Version              = "3.0.0-alpha-1";
     public const string LogFormat            = "[{@t:h:mm:ss ff tt}] [{@l:u3}]{#if EventId is not null} [{EventId.Name}]{#end} [{Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)}] {@m}\n{@x}";
     public const string FileLogFormat        = "[{Timestamp:h:mm:ss ff tt}] [{Level:u3}] [{SourceContext}] {Message:lj} {Exception:j}{NewLine}";
     public const string HttpClientName       = "Silk";
