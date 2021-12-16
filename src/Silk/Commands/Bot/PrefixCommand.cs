2928ae56945b9b959dba87f5886dea763d2594e6
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-16 15:05:04 -0500

    Add prefixes back

--- src/Silk/Commands/Bot/PrefixCommand.cs
+++ src/Silk/Commands/Bot/PrefixCommand.cs
@@ -1,71 +1,94 @@
-﻿//TODO: This
-/*using System.Linq;
+﻿using System.ComponentModel;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
+using MediatR;
+using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Commands.Conditions;
+using Remora.Discord.Commands.Contexts;
+using Remora.Results;
 using Silk.Data;
 using Silk.Data.Entities;
+using Silk.Data.MediatR.Guilds;
 using Silk.Services.Interfaces;
 using Silk.Utilities.HelpFormatter;
 using Silk.Extensions;
+using Silk.Extensions.Remora;
 using Silk.Shared.Constants;
 
 namespace Silk.Commands.Bot;
 
 [HelpCategory(Categories.Bot)]
-[ModuleLifespan(ModuleLifespan.Transient)]
-public class PrefixCommand : BaseCommandModule
+public class PrefixCommand : CommandGroup
 {
-    private const    int                 PrefixMaxLength = 5;
-    private readonly GuildContext        _db;
-    private readonly IPrefixCacheService _prefixCache;
-
-    public PrefixCommand(IPrefixCacheService prefixCache, GuildContext db)
+    private const int PrefixMaxLength = 5;
+    
+    private readonly IMediator              _mediator;
+    private readonly ICommandContext        _context;
+    private readonly IPrefixCacheService    _cache;
+    private readonly IDiscordRestGuildAPI   _guilds;
+    private readonly IDiscordRestChannelAPI _channels;
+    
+    public PrefixCommand
+    (
+        IMediator mediator,
+        ICommandContext context,
+        IPrefixCacheService cache,
+        IDiscordRestGuildAPI guilds,
+        IDiscordRestChannelAPI channels
+    )
     {
-        _prefixCache = prefixCache;
-        _db          = db;
+        _mediator = mediator;
+        _context  = context;
+        _cache    = cache;
+        _guilds   = guilds;
+        _channels = channels;
     }
+
+
 
     [Command("prefix")]
-    [Description("Sets the command prefix for Silk to use on the current Guild")]
-    public async Task SetPrefix(CommandContext ctx, string prefix)
+    [RequireContext(ChannelContext.Guild)]
+    [Description("Gets or sets the current prefix for the guild.")]
+    public async Task<Result<IMessage>> SetPrefix(string prefix = "")
     {
-        if (!ctx.Member.HasPermission(FlagConstants.CacheFlag)) return;
+        var member = await _guilds.GetGuildMemberAsync(_context.GuildID.Value, _context.User.ID, this.CancellationToken);
 
-        (bool valid, string reason) = IsValidPrefix(prefix);
-        if (!valid)
-        {
-            await ctx.RespondAsync(reason);
-            return;
-        }
+        if (!member.IsSuccess)
+            return Result<IMessage>.FromError(member.Error);
 
-        GuildEntity guild = _db.Guilds.First(g => g.Id == ctx.Guild.Id);
-        guild.Prefix = prefix;
-        _prefixCache.UpdatePrefix(ctx.Guild.Id, prefix);
+        var permissionResult = await member.Entity.HasPermissionAsync(_guilds, _context.GuildID.Value, DiscordPermission.ManageChannels);
+        
+        if (!permissionResult.IsSuccess)
+            return Result<IMessage>.FromError(permissionResult.Error);
 
-        await _db.SaveChangesAsync();
-        await ctx.RespondAsync($"Done! I'll respond to `{prefix}` from now on.");
+        if (!permissionResult.Entity || string.IsNullOrEmpty(prefix))
+            return await _channels.CreateMessageAsync(_context.ChannelID, $"I respond to {_cache.RetrievePrefix(_context.GuildID.Value)}, `/commands` and when you ping me!");
+        
+
+        var prefixCheckResult = IsValidPrefix(prefix);
+
+        if (!prefixCheckResult.IsSuccess)
+            return await _channels.CreateMessageAsync(_context.ChannelID, prefixCheckResult.Error.Message);
+
+        await _mediator.Send(new UpdateGuild.Request(_context.GuildID.Value, prefix));
+        
+        _cache.UpdatePrefix(_context.GuildID.Value, prefix);
+        
+        return await _channels.CreateMessageAsync(_context.ChannelID, $"Done! I'll respond to `{prefix}` from now on.");
     }
 
-    [Command("prefix")]
-    [Description("Gets Silk's command prefix for the current Guild")]
-    public async Task GetPrefix(CommandContext ctx)
-    {
-        string prefix = _prefixCache.RetrievePrefix(ctx.Guild?.Id);
-        await ctx.RespondAsync($"My prefix is `{prefix}`, but you can always use commands by mentioning me! ({ctx.Client.CurrentUser.Mention})");
-    }
-
-    private PrefixValidationResult IsValidPrefix(string prefix)
+    private Result IsValidPrefix(string prefix)
     {
         if (prefix.Length > PrefixMaxLength)
-            return new(false, $"Prefix cannot be more than {PrefixMaxLength} characters!");
+            return Result.FromError(new ArgumentOutOfRangeError( nameof(prefix), $"Prefix cannot be more than {PrefixMaxLength} characters!"));
 
-        if (!Regex.IsMatch(prefix, "[A-Z!@#$%^&*<>?.]+", RegexOptions.IgnoreCase))
-            return new(false, "Invalid prefix! `[Valid symbols: ! @ # $ % ^ & * < > ? / and A-Z (Case insensitive)]`");
+        if (!Regex.IsMatch(prefix, "[A-Z!@#$%^&*<>?.]{1,5}", RegexOptions.IgnoreCase))
+            return Result.FromError(new ArgumentInvalidError(nameof(prefix),  "Prefix contained an invalid symbol. Valid symbols include ! @ # $ % ^ & * < > ? and A-z (Case insensitive)"));
 
-        return new(true, "");
+        return Result.FromSuccess();
     }
-
-    private record PrefixValidationResult(bool Valid, string Reason);
-}*/
+}
