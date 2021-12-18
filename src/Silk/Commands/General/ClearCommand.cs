216fa0b4f1a213be2a33441ef6f108994debd2af
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-17 22:31:43 -0500

    Improved clear command

--- src/Silk/Commands/General/ClearCommand.cs
+++ src/Silk/Commands/General/ClearCommand.cs
@@ -1,73 +1,146 @@
-﻿//TODO: This
-/*using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
-using DSharpPlus.Exceptions;
-using MediatR;
-using Silk.Data.Entities;
-using Silk.Data.MediatR.Guilds;
+using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Commands.Conditions;
+using Remora.Discord.Commands.Contexts;
+using Remora.Rest.Core;
+using Remora.Results;
 using Silk.Utilities.HelpFormatter;
-using Silk.Shared.Constants;
 
 namespace Silk.Commands.General;
 
 [HelpCategory(Categories.Mod)]
-public class ClearCommand : BaseCommandModule
+public class ClearCommand : CommandGroup
 {
-    private readonly IMediator _mediator;
-
-    public ClearCommand(IMediator mediator) => _mediator = mediator;
-
-    [Command]
-    [Description("Cleans all messages from all users.")]
-    [RequireUserPermissions(Permissions.ManageMessages)]
-    public async Task Clear(CommandContext ctx, int numOfMessages = 5)
+    private readonly ICommandContext        _context;
+    private readonly IDiscordRestChannelAPI _channels;
+    public ClearCommand(ICommandContext context, IDiscordRestChannelAPI channels)
     {
-        IReadOnlyList<DiscordMessage> queriedMessages = await ctx.Channel.GetMessagesAsync(numOfMessages + 1);
-
-        var commandIssuingUser = $"{ctx.User.Username}{ctx.User.Discriminator}";
-        await ctx.Channel.DeleteMessagesAsync(queriedMessages, $"{commandIssuingUser} called clear command.");
-
-        DiscordEmbedBuilder? responseEmbed = MakeResponseEmbed(ctx, numOfMessages);
-        DiscordMessage       responseMsg   = await ctx.RespondAsync(responseEmbed);
-
-        GuildModConfigEntity guildConfig    = await GetOrCreateGuildConfig(ctx);
-        DiscordChannel?      loggingChannel = ctx.Guild.GetChannel(guildConfig.LoggingChannel);
-
-        DiscordEmbedBuilder? clearedMessagesEmbed = MakeLoggingChannelEmbed(ctx, numOfMessages);
-        if (loggingChannel is not null) await loggingChannel.SendMessageAsync(clearedMessagesEmbed);
-
-        await Task.Delay(5000);
-        try { await ctx.Channel.DeleteMessageAsync(responseMsg); }
-        catch (NotFoundException) { }
+        _context  = context;
+        _channels = channels;
     }
 
-    private static DiscordEmbedBuilder MakeResponseEmbed(CommandContext ctx, int messages) =>
-        new DiscordEmbedBuilder()
-           .WithAuthor(ctx.Member.DisplayName, null, ctx.Member.AvatarUrl)
-           .WithColor(DiscordColor.SpringGreen)
-           .WithDescription($"Cleared {messages} messages!");
 
-    private static DiscordEmbedBuilder MakeLoggingChannelEmbed(CommandContext ctx, int messages) =>
-        new DiscordEmbedBuilder()
-           .WithTitle("Cleared Messages:")
-           .WithDescription(
-                            $"User: {ctx.User.Mention}\n"       +
-                            $"Channel: {ctx.Channel.Mention}\n" +
-                            $"Amount: **{messages}**")
-           .AddField("User ID:", ctx.User.Id.ToString(), true)
-           .WithThumbnail(ctx.Message.Author.AvatarUrl)
-           .WithFooter("Cleared Messages at (UTC)")
-           .WithTimestamp(DateTime.Now.ToUniversalTime())
-           .WithColor(DiscordColor.Red);
-
-    private async Task<GuildModConfigEntity> GetOrCreateGuildConfig(CommandContext ctx)
+    [Command("clear")]
+    [Description("Cleans all messages from all users.\n"                   +
+                 "It's important to understand the order of precedence:\n" +
+                 "Skip ➜ User ➜ Regex\n"                                   +
+                 "This command evaluates the messages to skip, "           +
+                 "then filters by user (if specified), "                   +
+                 "and then by regex (if specified).\n\n"                   +
+                 "Furthermore, **--around and --skip are mutually exclusive.**")]
+    [RequireDiscordPermission(DiscordPermission.ManageMessages)]
+    [SuppressMessage("ReSharper", "RedundantBlankLines", Justification = "Readability")]
+    public async Task<Result<IMessage>> Clear
+    (
+        [Description("How many messages to delete. `--around` limits to 100.")]
+        int messageCount = 5,
+        
+        [Option('s', "skip")]
+        [Description("The amount of messages to skip.")]
+        int? skip = null,
+        
+        [Option('u', "user")]
+        [Description("The user to filter by.")]
+        IUser? user = null,
+        
+        [Option('r', "regex")]
+        [Description("The regex to filter by.")]
+        string? pattern = null,
+        
+        [Option('a', "around")]
+        [Description("Delete messages around the specified message.")]
+        IMessage? message = null
+    )
     {
-        GuildEntity guild = await _mediator.Send(new GetOrCreateGuildRequest(ctx.Guild.Id, StringConstants.DefaultCommandPrefix));
-        return guild.ModConfig;
+        if (skip is not null && message is not null)
+            return await _channels.CreateMessageAsync(_context.ChannelID, "You can only specify --skip **or** --around.");
+        
+        if (message?.ID.Timestamp.AddDays(14) < DateTimeOffset.UtcNow)
+            return await _channels.CreateMessageAsync(_context.ChannelID, "You can specify messages up to two weeks old.");
+        
+        var messageResult = await GetMessagesAsync(_context.ChannelID, message?.ID ?? default(Remora.Rest.Core.Optional<Snowflake>), messageCount + (skip + 1 ?? 1)); 
+            
+        if (!messageResult.IsSuccess)
+            return Result<IMessage>.FromError(messageResult.Error);
+        
+        var messages = messageResult.Entity.AsEnumerable();
+
+        messages = messages.Skip(skip + 1 ?? 1);
+
+        if (user is not null)
+            messages = messages.Where(x => x.Author.ID == user.ID);
+
+        if (pattern is not null)
+        {
+            var filterRegex = new Regex(pattern);
+            
+            messages = messages.Where(x => filterRegex.IsMatch(x.Content));
+        }
+        
+        messages = messages.Take(messageCount);
+        
+        var messageDeleteResult = await _channels.BulkDeleteMessagesAsync(_context.ChannelID, messages.Select(x => x.ID).ToArray());
+        
+        if (!messageDeleteResult.IsSuccess)
+            return Result<IMessage>.FromError(messageDeleteResult.Error);
+
+        var deleted = Math.Min(messageCount, messages.Count());
+        
+        var returnResult = await _channels.CreateMessageAsync(_context.ChannelID, $"Deleted {deleted} message{(deleted == 1 ? "" : "s")}.");
+        
+        await Task.Delay(6000);
+
+        await _channels.DeleteMessageAsync(_context.ChannelID, (_context as MessageContext)!.MessageID);
+        await _channels.DeleteMessageAsync(_context.ChannelID, returnResult.Entity.ID);
+
+        return returnResult;
     }
-}*/
+
+    /// <summary>
+    /// Gets the messages from the specified channel.
+    /// </summary>
+    /// <param name="channelID">The ID of the channel to fetch messages from.</param>
+    /// <param name="around">The ID of the message to fetch messages around</param>
+    /// <param name="limit">The limit of messages. If around is not specified, and this is greater than 100, the request will be paginated.</param>
+    /// <returns></returns>
+    private async Task<Result<IReadOnlyList<IMessage>>> GetMessagesAsync(Snowflake channelID, Remora.Rest.Core.Optional<Snowflake> around, int limit)
+    {
+        if (limit <= 100 || around.HasValue)
+            return await _channels.GetChannelMessagesAsync(channelID, around, limit: limit);
+
+        var messages = new List<IMessage>();
+        
+        var remaining = limit;
+        var before    = default(Remora.Rest.Core.Optional<Snowflake>);
+
+        while (remaining > 0)
+        {
+            var fetchResult = await _channels.GetChannelMessagesAsync(channelID, before, limit: 100);
+            
+            if (fetchResult.IsSuccess)
+            {
+                messages.AddRange(fetchResult.Entity);
+                remaining -= fetchResult.Entity.Count;
+                before    = fetchResult.Entity.Last().ID;
+                
+                if (fetchResult.Entity.Count < 100)
+                    break;
+            }
+            else
+            {
+                return fetchResult;
+            }
+        }
+        
+        return messages;
+    }
+}
