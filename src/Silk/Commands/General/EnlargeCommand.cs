d942d36fe087d9d4bb4c3773917ffe0d4fc1a41e
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-17 22:31:59 -0500

    Rewrite enlarge command

--- src/Silk/Commands/General/EnlargeCommand.cs
+++ src/Silk/Commands/General/EnlargeCommand.cs
@@ -1,11 +1,16 @@
-﻿using System.IO;
+﻿using System.ComponentModel;
+using System.IO;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
-using Silk.Utilities.HttpClient;
+using OneOf;
+using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Discord.API;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Commands.Contexts;
+using Remora.Results;
 using Silk.Utilities.HelpFormatter;
 using SkiaSharp;
 using Svg.Skia;
@@ -13,42 +18,66 @@ using Svg.Skia;
 namespace Silk.Commands.General;
 
 [HelpCategory(Categories.General)]
-public class EnlargeCommand : BaseCommandModule
+public class EnlargeCommand : CommandGroup
 {
-    private readonly IHttpClientFactory _httpClientFactory;
-    public EnlargeCommand(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
-
-    [Command("enlarge")] [Aliases("enbiggen", "emoji", "emote")]
-    [Description("Displays a larger version of the provided emoji or custom emote.")]
-    public async Task Enlarge(CommandContext ctx, DiscordEmoji emoji)
+    
+    private readonly HttpClient             _http;
+    private readonly ICommandContext       _context;
+    private readonly IDiscordRestChannelAPI _channels;
+    public EnlargeCommand
+    (
+        HttpClient http,
+        ICommandContext context,
+        IDiscordRestChannelAPI channels
+    )
     {
+        _http     = http;
+        _context  = context;
+        _channels = channels;
+    }
 
-        DiscordEmbedBuilder? embed = new DiscordEmbedBuilder().WithColor(new("2F3136"));
-        if (emoji.Id != 0) // Guild emote.
+    [Command("enlarge", "enbiggen", "emoji", "emote")]
+    [Description("Displays a larger version of the provided emoji or custom emote.")]
+    public async Task<Result<IMessage>> Enlarge(IPartialEmoji emoji)
+    {
+        if (emoji.ID.IsDefined()) // Guild emote.
         {
-            var message = new DiscordMessageBuilder();
-            embed.WithFooter(emoji.Name);
-            embed.WithImageUrl(emoji.Url + "?size=2048");
-            message.WithEmbed(embed)
-                   .WithReply(ctx.Message.Id);
-            await ctx.RespondAsync(message);
+            if (!emoji.ID.IsDefined(out var emojiID))
+                return Result<IMessage>.FromError(new ArgumentInvalidError(nameof(emoji), "The provided emoji is not a valid emote."));
+            
+            var emojiUriResult = CDN.GetEmojiUrl(emojiID.Value, imageSize: 256);
+
+            if (!emojiUriResult.IsDefined(out var emojiURI))
+                return Result<IMessage>.FromError(emojiUriResult.Error!);
+
+            var regexedEmojiURL = Regex.Match(emojiURI.ToString(), @"^https://cdn\.discordapp\.com/emojis/(?<EMOJI>\d+)\.(?<EXT>gif|png)(?:\?size\=\d+)?$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            
+            var emojiName = regexedEmojiURL.Groups["EMOJI"].Value + '.' + regexedEmojiURL.Groups["EXT"].Value;
+            
+            return await _channels.CreateMessageAsync
+                (
+                 _context.ChannelID,
+                 attachments: new OneOf<FileData, IPartialAttachment>[]
+                 {
+                     new FileData(emojiName, await _http.GetStreamAsync(emojiURI), null!)
+                 });
         }
-        else // Unicode emote.
+        else // Unicode emote.1
         {
-            await ctx.TriggerTypingAsync();
-
-            embed.WithFooter(Regex.Replace(emoji.GetDiscordName(), "(?<emote>:[A-z_-0-9])", "(?<emote>)"));
-            embed.WithImageUrl("attachment://emote.jpeg");
-
-            Stream? image = await RenderEmojiAsync(emoji.Name);
-
-            DiscordMessageBuilder? message = new DiscordMessageBuilder()
-                                            .WithEmbed(embed)
-                                            .WithFile("emote.jpeg", image)
-                                            .WithReply(ctx.Message.Id);
-
-            await message.SendAsync(ctx.Channel);
+            if (!emoji.Name.IsDefined(out var name))
+                return Result<IMessage>.FromError(new ArgumentInvalidError(nameof(emoji), "The provided emoji is not a valid emote."));
+            
+            return await _channels.CreateMessageAsync
+                (
+                 _context.ChannelID,
+                 attachments: new OneOf<FileData, IPartialAttachment>[]
+                 {
+                     new FileData(name + ".png", await RenderEmojiAsync(name), null!)
+                 }
+                );
         }
+        
+        return Result<IMessage>.FromError(new InvalidOperationError("Unable to find an emoji to enlarge."));
     }
 
     private async Task<Stream> RenderEmojiAsync(string unicodeEmoji)
@@ -58,11 +87,11 @@ public class EnlargeCommand : BaseCommandModule
 
         var emojiHex = char.ConvertToUtf32(unicodeEmoji, 0).ToString("X4");
         var url      = $"https://twemoji.maxcdn.com/2/svg/{emojiHex.ToLower()}.svg";
-        svgStream = await _httpClientFactory.CreateSilkClient().GetStreamAsync(url);
+        svgStream = await _http.GetStreamAsync(url);
 
         var svg = new SKSvg();
         svg.Load(svgStream);
-        svg.Save(imageStream, SKColor.Empty, scaleX: 16.0f, scaleY: 16.0f);
+        svg.Save(imageStream, SKColor.Empty, scaleX: 7.13f, scaleY: 7.13f);
 
         imageStream.Position = 0;
         return imageStream;
