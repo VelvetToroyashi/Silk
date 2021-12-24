5ae156e90ebe54f2048c38219055d33c52d2c601
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-24 18:21:38 -0500

    Implement ban and move clear command

--- src/Silk/Commands/Moderation/BanCommand.cs
+++ src/Silk/Commands/Moderation/BanCommand.cs
@@ -1,11 +1,14 @@
-﻿/*using System;
+﻿using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
+using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Commands.Conditions;
+using Remora.Discord.Commands.Contexts;
+using Remora.Results;
 using Silk.Services.Interfaces;
-using Silk.Types;
 using Silk.Utilities.HelpFormatter;
 using Silk.Extensions.DSharpPlus;
 
@@ -13,52 +16,59 @@ namespace Silk.Commands.Moderation
 {
     
     [HelpCategory(Categories.Mod)]
-    public class BanCommand : BaseCommandModule
+    public class BanCommand : CommandGroup
     {
-        private readonly IInfractionService _infractions;
-        public BanCommand(IInfractionService infractions) => _infractions = infractions;
-
-        [Command]
-        [RequireGuild]
-        [RequireBotPermissions(Permissions.BanMembers)]
-        [RequireUserPermissions(Permissions.BanMembers)]
-        [Description("Permanently ban someone from the server!")]
-        public async Task BanAsync(CommandContext ctx, DiscordUser user, [RemainingText] string reason = "Not Given.")
+        private readonly ICommandContext        _context;
+        private readonly IDiscordRestUserAPI    _users;
+        private readonly IDiscordRestChannelAPI _channels;
+        private readonly IInfractionService     _infractions;
+        
+        public BanCommand
+        (
+            ICommandContext context,
+            IDiscordRestUserAPI users,
+            IDiscordRestChannelAPI channels,
+            IInfractionService infractions
+        )
         {
-            if (user == ctx.Guild.CurrentMember)
-            {
-                await ctx.RespondAsync("Surely you didn't wanna get rid of me...Right?..Right?");
-                return;
-            }
+            _context     = context;
+            _users       = users;
+            _channels    = channels;
+            _infractions = infractions;
+        }
 
-            if (user == ctx.Member)
-            {
-                await ctx.RespondAsync("Are you *really* deserving of a ban, though?");
-                return;
-            }
+        [Command("ban")]
+        [RequireContext(ChannelContext.Guild)]
+        [RequireDiscordPermission(DiscordPermission.BanMembers)]
+        [Description("Permanently ban someone from the server!")]
+        public async Task<Result<IMessage>> BanAsync(IUser user, [Greedy] string reason = "Not Given.")
+        {
+            var self = await _users.GetCurrentUserAsync();
 
-            InfractionResult result = await _infractions.BanAsync(user.Id, ctx.Guild.Id, ctx.User.Id, reason);
+            if (self.IsSuccess)
+                return Result<IMessage>.FromError(self);
+            
+            if (user.ID == self.Entity.ID)
+                return await _channels.CreateMessageAsync(_context.ChannelID, "Sadly the ban hammer must come down upon someone else!");
 
-            string? message = result switch
-            {
-                InfractionResult.SucceededWithNotification    => $"Banned **{user.ToDiscordName()}** (User notified with Direct Message)",
-                InfractionResult.SucceededWithoutNotification => $"Banned **{user.ToDiscordName()}**! (Failed to DM.)",
-                InfractionResult.FailedGuildHeirarchy         => "I can't ban that person due to hierarchy.",
-                InfractionResult.FailedSelfPermissions        => "I don't know how you managed to do this, but I don't have permission to ban that person!"
-            };
+            if (user.ID == _context.User.ID)
+                return await _channels.CreateMessageAsync(_context.ChannelID,"The ban hammer must swing upon someone else!");
 
-            await ctx.RespondAsync(message);
+            var infractionResult = await _infractions.BanAsync(_context.GuildID.Value, user.ID, _context.User.ID, 0, reason);
+
+            return infractionResult.IsSuccess
+                ? await _channels.CreateMessageAsync(_context.ChannelID, $"Successfully banned <@{user.ID}>!")
+                : await _channels.CreateMessageAsync(_context.ChannelID, infractionResult.Error.Message);
         }
 
 
-        [Command]
-        [RequireGuild]
-        [RequireBotPermissions(Permissions.BanMembers)]
-        [RequireUserPermissions(Permissions.BanMembers)]
-        [Description("Permanently ban someone from the server!")]
-        public async Task TempBanAsync(CommandContext ctx, DiscordUser user, TimeSpan duration, [RemainingText] string reason = "Not Given.")
+        [Command("ban")]
+        [RequireContext(ChannelContext.Guild)]
+        [RequireDiscordPermission(DiscordPermission.BanMembers)]
+        [Description("Temporarily ban someone from the server!")]
+        public async Task TempBanAsync(CommandContext ctx, IUser user, TimeSpan duration, [Greedy] string reason = "Not Given.")
         {
-            if (user == ctx.Guild.CurrentMember)
+            /*if (user == ctx.Guild.CurrentMember)
             {
                 await ctx.RespondAsync("Surely you didn't wanna get rid of me...Right?..Right?");
                 return;
@@ -80,7 +90,7 @@ namespace Silk.Commands.Moderation
                 InfractionResult.FailedSelfPermissions        => "I don't know how you managed to do this, but I don't have permission to ban that person!"
             };
 
-            await ctx.RespondAsync(message);
+            await ctx.RespondAsync(message);*/
         }
     }
-}*/
+}
