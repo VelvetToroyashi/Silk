482c76072289b16a869a2058209666e6772b71ec
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-26 16:31:08 -0500

    Finish caching* and add checks

--- src/Silk/Services/Data/GuildCacherService.cs
+++ src/Silk/Services/Data/GuildCacherService.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
+using Humanizer;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Remora.Discord.API.Abstractions.Gateway.Events;
@@ -11,16 +14,26 @@ using Remora.Discord.API.Objects;
 using Remora.Rest.Core;
 using Remora.Results;
 using Silk.Data.MediatR.Guilds;
+using Silk.Data.MediatR.Users;
 using Silk.Shared.Constants;
 
 namespace Silk.Services.Data;
 
 public class GuildCacherService
 {
-    private const string GuildJoinThankYouMessage = "Hiya! My name is Silk! I hope to satisfy your entertainment and moderation needs.\n\n" +
-                                                    $"I respond to mentions and `{StringConstants.DefaultCommandPrefix}` by default, "      +
-                                                    $"but you can change that with `{StringConstants.DefaultCommandPrefix}prefix`\n\n"      +
-                                                    "There's also a variety of :sparkles: slash commands :sparkles: if those suit your fancy!";
+    /// <summary>
+    /// The time in which a guild is considered new, if the joined timestamp is within this threshold.
+    /// </summary>
+    // If we joined within the last 30 seconds, we consider it new.
+    // This is bound to change in the future. For now 30s is good enough to allow for caching,
+    // as well as accomodating for any responder delays.
+    public TimeSpan JoinedTimestampThreshold { get; set; } = 30.Seconds(); 
+
+    private const string GuildJoinThankYouMessage = "Hiya! My name is Silk! I hope to satisfy your entertainment and moderation needs.\n\n"      +
+                                                    $"I respond to mentions and `{StringConstants.DefaultCommandPrefix}` by default, "           +
+                                                    $"but you can change that with `{StringConstants.DefaultCommandPrefix}prefix`\n\n"           +
+                                                    "There's also a variety of :sparkles: slash commands :sparkles: if those suit your fancy!\n" +
+                                                    "(Currently unavailable in V3, sorry! :c)";
     private readonly IDiscordRestChannelAPI _channelApi;
     private readonly IDiscordRestGuildAPI   _guildApi;
 
@@ -42,16 +55,16 @@ public class GuildCacherService
     /// <summary>
     ///     Required permissions to send a welcome message to a new guild.
     /// </summary>
-    private readonly DiscordPermissionSet _welcomeMessagePermissions = new(DiscordPermission.SendMessages, DiscordPermission.EmbedLinks);
+    private readonly DiscordPermission _welcomeMessagePermissions = (DiscordPermission)((int)DiscordPermission.SendMessages | (int)DiscordPermission.EmbedLinks);
 
     public GuildCacherService
-        (
-            IMediator                   mediator,
-            IDiscordRestUserAPI         userApi,
-            IDiscordRestGuildAPI        guildApi,
-            IDiscordRestChannelAPI      channelApi,
-            ILogger<GuildCacherService> logger
-        )
+    (
+        IMediator                   mediator,
+        IDiscordRestUserAPI         userApi,
+        IDiscordRestGuildAPI        guildApi,
+        IDiscordRestChannelAPI      channelApi,
+        ILogger<GuildCacherService> logger
+    )
     {
         _mediator   = mediator;
         _userApi    = userApi;
@@ -95,7 +108,7 @@ public class GuildCacherService
                                                                                          guild.Roles.Where(r => currentMember.Roles.Contains(r.ID)).ToArray(),
                                                                                          overwrites);
 
-            if (permissions.HasPermission(DiscordPermission.SendMessages) && permissions.HasPermission(DiscordPermission.EmbedLinks))
+            if (permissions.HasPermission(_welcomeMessagePermissions))
             {
                 availableChannel = channel;
                 break;
@@ -104,7 +117,7 @@ public class GuildCacherService
 
         if (availableChannel is null)
         {
-            _logger.LogInformation("Unable to find suitable channel. Guild likely has verification.");
+            _logger.LogWarning("Unable to find open channel to send thank you message to in guild {GuildID}", guild.ID);
         }
         else
         {
@@ -112,7 +125,7 @@ public class GuildCacherService
 
             if (send.IsSuccess)
             {
-                _logger.LogInformation("Sent thank you message to new guild.");
+                _logger.LogInformation("Sent thank you message to new guild: {GuildID}", guild.ID);
                 return Result.FromSuccess();
             }
         }
@@ -125,29 +138,21 @@ public class GuildCacherService
     /// </summary>
     /// <param name="ready">The gateway event to store IDs from.</param>
     /// <returns></returns>
-    public Result StoreKnownGuilds(IReady ready)
+    public void StoreKnownGuilds(IReady ready)
     {
-        var failed = false;
-        var errors = new List<IResult>();
-
         foreach (IUnavailableGuild guild in ready.Guilds)
-        {
-            if (!_knownGuilds.Add(guild.GuildID))
-            {
-                failed = true;
-                errors.Add(Result.FromError(new InvalidOperationError($"Guild {guild.GuildID} was already known.")));
-            }
-        }
-
-        return failed ? Result.FromError(new AggregateError(errors, "One or more guilds were already known.")) : Result.FromSuccess();
+            _knownGuilds.Add(guild.ID);
     }
 
     /// <summary>
     ///     Checks whether a guild was just joined, or if it was already joined.
     /// </summary>
     /// <param name="guildID">The ID of the guild to check.</param>
-    /// <returns></returns>
-    public bool IsNewGuild(Snowflake guildID) => !_knownGuilds.Contains(guildID);
+    // This technically introduces an edge case where the bot isn't on any guilds,
+    // and the first guild would be considered old, but the first guild a bot joins is likely a testing server anyway,
+    // therefore there's no reason to send a greeting there. This also helps prevent calling this before the bot is ready.
+    public bool IsNewGuild(Snowflake guildID) 
+        => _knownGuilds.Any() && !_knownGuilds.Contains(guildID);
 
     public async Task<Result> CacheGuildAsync(Snowflake guildID, IReadOnlyList<IGuildMember> members)
     {
@@ -158,11 +163,10 @@ public class GuildCacherService
 
         return default;
     }
-
-
-    public async Task<Result> CacheMembersAsync(Snowflake guildID, IReadOnlyList<IGuildMember> members)
+    
+    private async Task<Result> CacheMembersAsync(Snowflake guildID, IReadOnlyList<IGuildMember> members)
     {
-        if (members.Count > 2) // Just us, or just a bad collection. Either way this isn't a valid state.
+        if (members.Count < 2) // Just us, or just a bad collection. Either way this isn't a valid state.
             return Result.FromError(new ArgumentOutOfRangeError("Members only contained current user."));
 
         var erroredMembers = new List<IResult>();
@@ -174,10 +178,13 @@ public class GuildCacherService
                 erroredMembers.Add(Result.FromError(new InvalidOperationError("Member did not have a defined user.")));
                 continue;
             }
-            //TODO: FINISH THIS
+            
+            var currentMemberState = await _mediator.Send(new GetOrCreateUserRequest(guildID, user.ID, JoinedAt: member.JoinedAt));
+
+            if (!currentMemberState.IsSuccess)
+                erroredMembers.Add(currentMemberState);
         }
-
-
+        
         return erroredMembers.Any()
             ? Result.FromError(new AggregateError(erroredMembers, "One or more guild members could not be cached."))
             : Result.FromSuccess();
