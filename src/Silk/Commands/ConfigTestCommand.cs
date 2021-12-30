26507017e530d00883e3dd70e28c753f56e28e0f
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-30 18:42:54 -0500

    Comment out ConfigTestCommand. It's just for reference.

--- src/Silk/Commands/ConfigTestCommand.cs
+++ src/Silk/Commands/ConfigTestCommand.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel;
+/*using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using MediatR;
@@ -15,17 +15,7 @@ using Silk.Data.MediatR.Guilds.Config;
 
 namespace Silk.Commands;
 
-public class A : CommandGroup
-{
-    [Command(nameof(A1))]
-    public async Task<Result> A1() => Result.FromSuccess();
-    
-    [Command(nameof(A1))]
-    public async Task<Result> A1(IUser user) => Result.FromSuccess();
-    
-}
-
-[Group("config")]
+//[Group("config")]
 [Description("Configure various settings for your guild!")]
 //[RequireDiscordPermission(DiscordPermission.ManageGuild)]
 
@@ -151,4 +141,4 @@ public class ConfigTestCommand : CommandGroup
             };
         }
     }
-}
+}*/
