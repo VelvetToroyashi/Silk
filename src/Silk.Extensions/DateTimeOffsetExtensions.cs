ab6425f7bcf554dd463fde592418fe3f70e3db5e
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-30 18:39:23 -0500

    Add DateTime and mentionable entity extensions

---
+++ src/Silk.Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Globalization;
+
+namespace Silk.Extensions;
+
+public enum TimestampFormat : byte
+{
+    Relative      = (byte)'R',
+    ShortDate     = (byte)'d',
+    LongDate      = (byte)'D',
+    ShortTime     = (byte)'t',
+    LongTime      = (byte)'T',
+    ShortDateTime = (byte)'f',
+    LongDateTime  = (byte)'F',
+}
+
+public static class DateTimeOffsetExtensions
+{
+    public static string ToTimestamp(this DateTimeOffset dto, TimestampFormat format = TimestampFormat.Relative)
+        => $"<t:{dto.ToUnixTimeSeconds()}:{(char)format}>";
+}
