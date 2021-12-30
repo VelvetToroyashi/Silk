82ddd70520f9a769c57a48c8ce27c3f44de57ea0
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-12-30 18:38:17 -0500

    Implement cases command (needs pagination)

--- src/Silk/Commands/Moderation/CasesCommand.cs
+++ src/Silk/Commands/Moderation/CasesCommand.cs
@@ -1,15 +1,23 @@
-﻿//TODO: This
-/*using System.Linq;
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
 using Humanizer;
 using MediatR;
+using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.API.Objects;
+using Remora.Discord.Commands.Conditions;
+using Remora.Discord.Commands.Contexts;
+using Remora.Results;
 using Silk.Data.Entities;
 using Silk.Data.MediatR.Infractions;
 using Silk.Data.MediatR.Users;
+using Silk.Extensions;
+using Silk.Interactivity;
 using Silk.Utilities;
 using Silk.Utilities.HelpFormatter;
 
@@ -17,58 +25,81 @@ namespace Silk.Commands.Moderation;
 
 
 [HelpCategory(Categories.Mod)]
-public class CasesCommand : BaseCommandModule
+public class CasesCommand : CommandGroup
 {
-    private readonly IMediator _mediator;
-    public CasesCommand(IMediator mediator) => _mediator = mediator;
-
-    [Command]
-    [RequireGuild]
+    private readonly IMediator              _mediator;
+    private readonly ICommandContext        _context;
+    private readonly IDiscordRestChannelAPI _channels;
     
-    [Description("Check the cases of a user including notes.")]
-    public async Task Cases(CommandContext ctx, DiscordUser user)
+    private readonly InteractivityExtension _interactivity;
+    
+    public CasesCommand(IMediator mediator, ICommandContext context, IDiscordRestChannelAPI channels, InteractivityExtension interactivity)
     {
-        DiscordMessageBuilder? mBuilder = new DiscordMessageBuilder().WithReply(ctx.Message.Id);
-        var                    eBuilder = new DiscordEmbedBuilder();
-
-        InfractionEntity[]? infractions = (await _mediator.Send(new GetUserInfractionsRequest(ctx.Guild.Id, user.Id)))?.ToArray();
-        bool                userExists  = await _mediator.Send(new GetUserRequest(ctx.Guild.Id, user.Id)) is not null;
-
-        if (!userExists || infractions.Length is 0)
-        {
-            mBuilder.WithContent("User has no cases!");
-            await ctx.RespondAsync(mBuilder);
-        }
-        else
-        {
-            var stringBuilder = new StringBuilder();
-            foreach (InfractionEntity currentInfraction in infractions)
-            {
-                stringBuilder
-                   .Append($"Case {currentInfraction.CaseNumber}: ")
-                   .Append($"`{currentInfraction.Type.Humanize(LetterCasing.Title)}`\n");
-                if (currentInfraction.Escalated)
-                    stringBuilder.AppendLine("\n[ESCALATED FROM STRIKE] ");
-
-
-                string? reason =
-                    currentInfraction.Reason.Length <= 200 ?
-                        $"Reason: {currentInfraction.Reason}" :
-                        $"Reason: {currentInfraction.Reason[..200]}";
-                stringBuilder.AppendLine(reason);
-            }
-
-            eBuilder
-               .WithColor(DiscordColor.Gold)
-               .WithTitle($"Cases for {user.Id}")
-               .WithDescription(stringBuilder.ToString());
-            mBuilder.WithEmbed(eBuilder);
-
-            await ctx.RespondAsync(mBuilder);
-
-            /*
-             * TODO: Pagination
-             #1#
-        }
+        _mediator           = mediator;
+        _context            = context;
+        _channels           = channels;
+        _interactivity = interactivity;
     }
-}*/
+
+    [Command("case")]
+    [RequireContext(ChannelContext.Guild)]
+    [Description("View information about a specific case.")]
+    [RequireDiscordPermission(DiscordPermission.ManageMessages)]
+    public async Task<Result<IMessage>> ViewCaseAsync(int caseID)
+    {
+        var infCase = await _mediator.Send(new GetUserInfractionRequest(default, _context.GuildID.Value, default, caseID));
+        
+        if (infCase is null)
+            return await _channels.CreateMessageAsync(_context.ChannelID, "Case not found.");
+
+        var embed = new Embed
+        {
+            Title       = $"Case { caseID } - {infCase.Type.Humanize(LetterCasing.Title)}",
+            Colour      = Color.Goldenrod,
+            Description = $"**Reason:** {infCase.Reason}",
+            Fields = new IEmbedField[]
+            {
+                new EmbedField("Target:", $"{infCase.TargetID}\n<@{infCase.TargetID}>", true),
+                new EmbedField("Enforcer:", $"{infCase.EnforcerID}\n<@{infCase.EnforcerID}>", true),
+                new EmbedField("Duration:", infCase.Duration?.Humanize() ?? "Permanent", true),
+                new EmbedField("Created:", infCase.CreatedAt.ToTimestamp(),true),
+                new EmbedField("Expires:", infCase.ExpiresAt?.Humanize() ?? "Never", true),
+                new EmbedField("Pardoned:", (!infCase.AppliesToTarget).ToString(), true)
+            }
+        };
+        
+        //TODO: Pagination, waiting on Remora to support it
+        
+        return await _channels.CreateMessageAsync(_context.ChannelID, embeds: new[] { embed });
+    }
+
+    
+
+    [Command("cases")]
+    [RequireContext(ChannelContext.Guild)]
+    [RequireDiscordPermission(DiscordPermission.ManageMessages)]
+    [Description("Fetch all infractions for a user including kicks, mutes, and more.")]
+    public async Task<Result<IMessage>> Cases(IUser user)
+    {
+        var cases = await _mediator.Send(new GetUserInfractionsRequest(_context.GuildID.Value, user.ID));
+        
+        if (!cases.Any())
+            return await _channels.CreateMessageAsync(_context.ChannelID, "It appears this user is clean. They should keep it up!");
+
+        var embed = new Embed()
+        {
+            Title       = $"Infractions for {user.Username}#{user.Discriminator:0000}",
+            Colour      = Color.Goldenrod,
+            Description = cases.Select(GetCaseDescription).Join("\n")
+        };
+        
+        return await _channels.CreateMessageAsync(_context.ChannelID, embeds: new[] {embed});
+    }
+
+    private string GetCaseDescription(InfractionEntity infraction) =>
+        $"| {infraction.CaseNumber} "                            +
+        $"| **{infraction.Type.Humanize(LetterCasing.Title)}** " +
+        $"| {infraction.CreatedAt.ToTimestamp()} "               +
+        $"| {infraction.EnforcerID} "                            +
+        $"|\n Reason: {infraction.Reason.Truncate(150, " [...]")}";
+}
