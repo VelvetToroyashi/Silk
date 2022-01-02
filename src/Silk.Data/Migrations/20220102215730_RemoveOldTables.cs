3589aa3876fbe4282ba4e1380902ae801d32a801
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-01-02 17:18:28 -0500

    Manually drop tables. I love Postgres

---
+++ src/Silk.Data/Migrations/20220102215730_RemoveOldTables.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Silk.Data.Migrations
+{
+    public partial class RemoveOldTables : Migration
+    {
+        //Manually added. Probably not a good idea.
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+@"DROP TABLE IF EXISTS
+    ""CommandInvocations"",
+    ""DisabledCommandEntity"",
+    ""GuildConfigs"",
+    ""GuildGreetingEntity"",
+    ""GuildLoggingConfigEntity"",
+    ""GuildModConfigs"",
+    ""Guilds"",
+    ""Infractions"",
+    ""InfractionStepEntity"",
+    ""InviteEntity"",
+    ""LoggingChannelEntity"",
+    ""Reminders"",
+    ""Tags"",
+    ""UserHistoryEntity"",
+    ""Users""
+    CASCADE;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
