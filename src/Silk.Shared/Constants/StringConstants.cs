3c5802fc3bfcfde66ce85e482a6a09ffbfe255e9
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-01-10 02:42:09 -0500

    [build core] ????

--- src/Silk.Shared/Constants/StringConstants.cs
+++ src/Silk.Shared/Constants/StringConstants.cs
@@ -4,7 +4,7 @@
 	{
 
 		public const string DefaultCommandPrefix = "s!";
-		public const string Version = "2.2.16-hotfix-2";
+		public const string Version = "2.2.16-hotfix-3";
 		public const string LogFormat = "[{@t:h:mm:ss ff tt}] [{@l:u3}]{#if EventId is not null} [{EventId.Name}]{#end} [{Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)}] {@m}\n{@x}";
 		public const string FileLogFormat = "[{Timestamp:h:mm:ss ff tt}] [{Level:u3}] [{SourceContext}] {Message:lj} {Exception:j}{NewLine}";
 		public const string HttpClientName = "Silk";
