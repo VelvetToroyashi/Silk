c34acf346187a4db5422ce99a923458401bb6511
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2022-02-19 12:34:52 -0500

    Initial work on GreetingEditor (pain)

---
+++ src/Silk.Dashboard/Components/GreetingEditor/GreetingEditor.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Remora.Rest.Core;
+using Silk.Dashboard.Converters;
+using Silk.Data.Entities;
+
+namespace Silk.Dashboard.Components;
+
+public partial class GreetingEditor
+{
+    private const string IdValidatorError = "Not a valid ID";
+
+    private MudForm                                 _form;
+
+    private readonly GreetingOption[] _greetingOptions = Enum.GetValues<GreetingOption>();
+
+    [Parameter]
+    public GuildGreetingEntity Greeting { get; set; } = new();
+
+    private async Task SubmitAsync()
+    {
+    }
+}
---
+++ src/Silk.Dashboard/Converters/MudBlazorSnowflakeConverter.cs
@@ -0,0 +1,46 @@
+﻿using Remora.Discord.API;
+using Remora.Rest.Core;
+
+namespace Silk.Dashboard.Converters;
+
+public static class MudBlazorSnowflakeConverter
+{
+    private const string IdValidationError = "Not a valid ID";
+
+    public static NullableSnowflakeConverter    NullableConverter    = new();
+    public static NonNullableSnowflakeConverter NonNullableConverter = new();
+
+    public static Func<Snowflake, string> NonNullableValidator =
+        s => s != default ? null : IdValidationError;
+
+    public static Func<Snowflake?, string> NullableValidator =
+        s => s?.Value != default ? null : IdValidationError;
+
+    public class NonNullableSnowflakeConverter : MudBlazor.Converter<Snowflake>
+    {
+        public NonNullableSnowflakeConverter()
+        {
+            SetFunc = snowflake => snowflake.Value.ToString();
+            GetFunc = ConvertFrom;
+        }
+
+        private Snowflake ConvertFrom(string value)
+        {
+            var r = Snowflake.TryParse(value, out var snowflake, Constants.DiscordEpoch)
+                ? snowflake.Value
+                : default;
+            return r;
+        }
+    }
+
+    public class NullableSnowflakeConverter : MudBlazor.Converter<Snowflake?>
+    {
+        public NullableSnowflakeConverter()
+        {
+            SetFunc = snowflake => snowflake?.Value.ToString();
+            GetFunc = value => Snowflake.TryParse(value, out var snowflake, Constants.DiscordEpoch)
+                ? snowflake
+                : default;
+        }
+    }
+}
