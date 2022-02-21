22da49c2e102c566a439939509bd437bf0f67176
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2022-02-20 21:34:12 -0500

    Remove un-needed usings + properties from GreetingEditor

--- src/Silk.Dashboard/Components/GreetingEditor/GreetingEditor.razor.cs
+++ src/Silk.Dashboard/Components/GreetingEditor/GreetingEditor.razor.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MudBlazor;
-using Remora.Rest.Core;
-using Silk.Dashboard.Converters;
 using Silk.Data.Entities;
 
 namespace Silk.Dashboard.Components;
 
 public partial class GreetingEditor
 {
-    private const string IdValidatorError = "Not a valid ID";
-
     private MudForm                                 _form;
 
     private readonly GreetingOption[] _greetingOptions = Enum.GetValues<GreetingOption>();
