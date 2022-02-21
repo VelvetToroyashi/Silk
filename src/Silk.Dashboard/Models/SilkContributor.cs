d4511f23c2830be560a4b5f91491aaee241b1ce1
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2022-02-21 03:32:23 -0500

    Fetch contributors from Github repo for About page, set default text color to white in ThemeManager

--- src/Silk.Dashboard/Models/SilkContributor.cs
+++ src/Silk.Dashboard/Models/SilkContributor.cs
@@ -1,7 +1,25 @@
-﻿namespace Silk.Dashboard.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Silk.Dashboard.Models;
 
 /* Todo: Move this when ready to the Shared project (keeping here for now) */
-public record SilkContributor(string Name, string Description, string ImageUrl)
+public record SilkContributor
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
+    [JsonPropertyName("login")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("avatar_url")]
+    public string AvatarUrl { get; set; }
+
+    [JsonPropertyName("html_url")]
+    public string HtmlUrl { get; set; }
+
+    [JsonPropertyName("contributions")]
+    public int Contributions { get; set; }
+
+    [JsonIgnore]
+    public string Description { get; set; } = "";
 }
---
+++ src/Silk.Dashboard/Pages/About.razor.cs
@@ -0,0 +1,70 @@
+﻿#nullable enable
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components;
+using Silk.Dashboard.Models;
+using Silk.Shared.Constants;
+
+namespace Silk.Dashboard.Pages;
+
+public partial class About
+{
+    [Inject]
+    private IHttpClientFactory HttpClientFactory { get; set; }
+
+    private const string ContributorsUrl         = "https://api.github.com/repos/VTPDevelopment/Silk/contributors";
+    private const string ExcludedContributorName = "[bot]";
+
+    private static readonly TimeSpan       _fetchPeriod = TimeSpan.FromHours(1);
+    private static          DateTimeOffset _nextFetchTime;
+    
+    private static readonly JsonSerializerOptions _serializerOptions = new()
+    {
+        AllowTrailingCommas         = true,
+        DefaultIgnoreCondition      = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNameCaseInsensitive = true,
+    };
+
+    // Todo: Paginate GET contributors (defaults to ~30)
+    private static List<SilkContributor>? _contributors;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await GetContributorsAsync();
+    }
+
+    private async Task GetContributorsAsync()
+    {
+        if (_contributors is null)
+        {
+            await FetchContributorsAsync();
+        }
+        else
+        {
+            if (_nextFetchTime < DateTimeOffset.UtcNow)
+                await FetchContributorsAsync();
+        }
+
+        _contributors = _contributors?.Where(contributor => !contributor.Name.Contains(ExcludedContributorName))
+                                      .OrderByDescending(contributor => contributor.Contributions)
+                                      .ToList();
+    }
+
+    private async Task FetchContributorsAsync()
+    {
+        using var client  = HttpClientFactory.CreateClient();
+        using var request = new HttpRequestMessage(HttpMethod.Get, ContributorsUrl);
+
+        request.Headers.TryAddWithoutValidation("Accept", "application/vnd.github.v3+json");
+        request.Headers.TryAddWithoutValidation("User-Agent", StringConstants.ProjectIdentifier);
+
+        using var result = await client.SendAsync(request);
+
+        if (!result.IsSuccessStatusCode) return;
+
+        _contributors  = JsonSerializer.Deserialize<List<SilkContributor>>(await result.Content.ReadAsStringAsync(), 
+                                                                           _serializerOptions);
+        _nextFetchTime = DateTimeOffset.UtcNow + _fetchPeriod;
+    }
+}
