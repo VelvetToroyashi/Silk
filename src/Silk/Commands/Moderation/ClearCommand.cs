bf31ad95828fc3042385a6f0cee2d9d48faf93f6
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-03-01 17:03:32 -0500

    About as consistent as Discord

--- src/Silk/Commands/Moderation/ClearCommand.cs
+++ src/Silk/Commands/Moderation/ClearCommand.cs
@@ -13,6 +13,7 @@ using Remora.Discord.Commands.Conditions;
 using Remora.Discord.Commands.Contexts;
 using Remora.Rest.Core;
 using Remora.Results;
+using Silk.Shared.Constants;
 using Silk.Utilities.HelpFormatter;
 
 namespace Silk.Commands.General;
@@ -102,7 +103,7 @@ public class ClearCommand : CommandGroup
 
         var deleted = Math.Min(messageCount, messages.Count());
         
-        var returnResult = await _channels.CreateMessageAsync(_context.ChannelID, $"Deleted {deleted} message{(deleted == 1 ? "" : "s")}.");
+        var returnResult = await _channels.CreateMessageAsync(_context.ChannelID, $"{Emojis.DeleteEmoji} Deleted {deleted} message{(deleted == 1 ? "" : "s")}.");
         
         await Task.Delay(6000);
 
