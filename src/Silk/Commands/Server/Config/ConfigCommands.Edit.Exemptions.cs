f238cd5c315b20eb5c84a6ed86f7f8dac4b4bd45
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-03-03 05:20:17 -0500

    feat: config view exemptions

--- src/Silk/Commands/Server/Config/ConfigCommands.Edit.Exemptions.cs
+++ src/Silk/Commands/Server/Config/ConfigCommands.Edit.Exemptions.cs
@@ -17,30 +17,27 @@ public partial class ConfigCommands
 {
     public partial class EditConfigCommands
     {
-        private const string ExemptionDescription = "Edits exemptions for your server. Exemptions allow targets to savely bypass specific automod actions (such as anti-spam)\n"                +
-                                                    "Valid types are [case-insensitive]:\n"                                                                                                     +
-                                                    $"{nameof(ExemptionCoverage.Invites)} - Exempts targets from being warned for sending invites (if configured)\n"                            +
-                                                    $"{nameof(ExemptionCoverage.Spam)} - Exempts targets from being warned for spamming (if configured)\n"                                      +
-                                                    $"{nameof(ExemptionCoverage.Phishing)} - Exempts targets from phishing actions. **It is strongly advised to only apply this to channels**." +
-                                                    $"{nameof(ExemptionCoverage.MessageEdits)} - Exempts targets from having their messages message edits logged (if configured)\n"             +
-                                                    $"{nameof(ExemptionCoverage.MessageDeletes)} - Exempts targets from having their messages message deletes logged (if configured)\n"         +
-                                                    "\"Target\" refers to the target of the exemption, beit a user, role, or channel.";
+        private const string ExemptionDescription =
+            "Exemptions allow targets to bypass automod (such as anti-spam)\n\n"                     +
+            "\"Target\" refers to the target of the exemption, be it a user, role, or channel.\n"    +
+            "Supports "                                                                              +
+            $"`{nameof(ExemptionCoverage.AntiInvite)}`, `{nameof(ExemptionCoverage.AntiPhishing)}` " +
+            $"`{nameof(ExemptionCoverage.EditLogging)}`, `{nameof(ExemptionCoverage.DeleteLogging)}`\n\n";
     
         [Command("exemptions", "exempt", "ex")]
         [Description(ExemptionDescription)]
         public async Task<IResult> EditExemptionsAsync
         (
             [Option("targets")] 
-            [Description("Targets to specified exemptions to. Specify roles, channels, or users.")]
+            [Description("Users, roles, and channels the exemption applies to.")]
             OneOf<IUser, IRole, IChannel>[] targets,
         
             [Option("add")]
-            [Description("Targets to add from the specified exemptions from. Specify a role, channel, or user.\n" +
-                         "Targets that have existing exemptions will be combined with the specified exemption types.")]
+            [Description("The exemption types to add to the targets.")]
             ExemptionCoverage[] add,
         
             [Option("remove")]
-            [Description($"The type(s) of exmeptions to remove.")] 
+            [Description("The exemption types to remove from the targets.")] 
             ExemptionCoverage[] remove
         )
         {
