d535ae383ca453121d58c1f2a9a0483038a1efaa
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-03-04 23:05:00 -0500

    feat: Add user cacher responder

---
+++ src/Silk/Responders/Guild/Member/MemberDataCacherResponder.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+using Silk.Data.MediatR.Users;
+using Silk.Data.MediatR.Users.History;
+
+namespace Silk.Responders;
+
+[ResponderGroup(ResponderGroup.Early)]
+public class MemberDataCacherResponder : IResponder<IGuildMemberAdd>, IResponder<IGuildMemberRemove>
+{
+    private readonly IMediator _mediator;
+    public MemberDataCacherResponder(IMediator mediator) => _mediator = mediator;
+
+    public async Task<Result> RespondAsync(IGuildMemberAdd gatewayEvent, CancellationToken ct = default)
+    {
+        var cacheResult = await _mediator.Send(new GetOrCreateUser.Request(gatewayEvent.GuildID, gatewayEvent.User.Value.ID, JoinedAt: gatewayEvent.JoinedAt), ct);
+
+        if (cacheResult.IsDefined(out var user) && user.History.JoinDates.Count > 1)
+            await _mediator.Send(new AddUserJoinDate.Request(gatewayEvent.GuildID, user.ID, gatewayEvent.JoinedAt), ct);
+
+        return cacheResult.IsSuccess ? Result.FromSuccess() : Result.FromError(cacheResult.Error);
+    }
+
+    public async Task<Result> RespondAsync(IGuildMemberRemove gatewayEvent, CancellationToken ct = default)
+    {
+        var cacheResult = await _mediator.Send(new GetOrCreateUser.Request(gatewayEvent.GuildID, gatewayEvent.User.ID, JoinedAt: DateTimeOffset.MinValue), ct);
+        
+        if (cacheResult.IsDefined(out var user) && user.History.LeaveDates.Count > 1)
+            await _mediator.Send(new AddUserLeaveDate.Request(gatewayEvent.GuildID, user.ID, DateTimeOffset.UtcNow), ct);
+        
+        return cacheResult.IsSuccess ? Result.FromSuccess() : Result.FromError(cacheResult.Error);
+    }
+}
