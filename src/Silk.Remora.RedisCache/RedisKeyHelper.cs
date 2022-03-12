dc0344d5af69d30a6533228dd02dc56dc25e6adc
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-03-12 13:18:53 -0500

    Add lot more keys

--- src/Silk.Remora.RedisCache/RedisKeyHelper.cs
+++ src/Silk.Remora.RedisCache/RedisKeyHelper.cs
@@ -7,31 +7,41 @@ public static class RedisKeyHelper
 {
     public static string CreateChannelCacheKey(in Snowflake channelID) => $"Channel:{channelID}";
     
-    public static string CreateGuildCacheKey(in Snowflake guildID) => $"Guild:{guildID}";
+    public static string CreatePinnedMessagesCacheKey(in Snowflake channelID) => $"Channel:{channelID}:Messages:Pinned";
     
-    public static string CreateUserCacheKey(in Snowflake userID) => $"User:{userID}";
+    public static string CreateChannelInvitesCacheKey(in Snowflake channelID) => $"Channel:{channelID}:Invites";
+
+    public static string CreateChannelPermissionCacheKey(in Snowflake channelID, in Snowflake overwriteID) => $"Channel:{channelID}:Overwrite:{overwriteID}";
     
-    public static string CreateGuildRoleCacheKey(in Snowflake guildID, in Snowflake roleID) => $"Guild:{guildID}:Role:{roleID}";
+    public static string CreateCurrentUserCacheKey() => "User:@me";
     
     public static string CreateEmojiCacheKey(in Snowflake guildID, in Snowflake emojiID) => $"Guild:{guildID}:Emoji:{emojiID}";
     
-    public static string CreateGuildMemberCacheKey(in Snowflake guildID, in Snowflake userID) => $"GuildMember:{guildID}:{userID}";
-    
-    public static string CreateGuildRolesCacheKey(in Snowflake guildID) => $"Guild:{guildID}:Roles";
-   
-    public static string CreateGuildMembersCacheKey(in Snowflake guildID) => $"Guild:{guildID}:Members";
-    
-    public static string CreateGuildEmojisCacheKey(in Snowflake guildID) => $"Guild:{guildID}:Emojis";
+    public static string CreateGuildCacheKey(in Snowflake guildID) => $"Guild:{guildID}";
     
     public static string CreateGuildChannelsCacheKey(in Snowflake guildID) => $"Guild:{guildID}:Channels";
     
+    public static string CreateGuildEmojisCacheKey(in Snowflake guildID) => $"Guild:{guildID}:Emojis";
+    
+    public static string CreateGuildMemberCacheKey(in Snowflake guildID, in Snowflake userID) => $"Guild:{guildID}:Member:{userID}";
+    
+    public static string CreateGuildMembersCacheKey(in Snowflake guildID) => $"Guild:{guildID}:Members";
+    
+    public static string CreateGuildRoleCacheKey(in Snowflake guildID, in Snowflake roleID) => $"Guild:{guildID}:Role:{roleID}";
+    
+    public static string CreateGuildRolesCacheKey(in Snowflake guildID) => $"Guild:{guildID}:Roles";
+    
     public static string CreateGuildVoiceStatesCacheKey(in Snowflake guildID) => $"Guild:{guildID}:VoiceStates";
     
-    public static string CreatePresenceCacheKey(in Snowflake guildID, in Snowflake userID) => $"Guilds:{guildID}:Presences:{userID}";
-
-    public static string CreateCurrentUserCacheKey() => "User:@me";
-    
-    public static string CreateThreadMemberCacheKey(in Snowflake threadID, in Snowflake userID) => $"Thread:{threadID}:Members:{userID}";
+    public static string CreateInviteCacheKey(string code) => $"Invite:{code}";
     
     public static string CreateMessageCacheKey(in Snowflake channelID, in Snowflake messageID) => $"Channel:{channelID}:Message:{messageID}";
+    
+    public static string CreatePresenceCacheKey(in Snowflake guildID, in Snowflake userID) => $"Guilds:{guildID}:Member:{userID}:Presence";
+    
+    public static string CreateThreadMemberCacheKey(in Snowflake threadID, in Snowflake userID) => $"Thread:{threadID}:Member:{userID}";
+    
+    public static string CreateThreadMembersCacheKey(in Snowflake threadID) => $"Thread:{threadID}:Members";
+    
+    public static string CreateUserCacheKey(in Snowflake userID) => $"User:{userID}";
 }
