9d3c40747107b53db5edc6e190180ad1d78ad9c5
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-03-15 17:00:05 -0400

    [build core] bump bot version

--- src/Silk.Shared/Constants/StringConstants.cs
+++ src/Silk.Shared/Constants/StringConstants.cs
@@ -15,7 +15,7 @@ public static class StringConstants
     /// <summary>
     /// The current version of Silk!.
     /// </summary>
-    public const string Version              = "3.0.1-beta-1";
+    public const string Version              = "3.0.1-beta-2";
     
     /// <summary>
     /// A special expression based log template that allows for conditional event ID insertion.
