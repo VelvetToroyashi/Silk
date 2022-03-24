890ac42897226e130119d098fe921cfa04b9abc1
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-03-24 05:37:44 -0400

    Restrict throw command

--- src/Silk/Commands/ThrowCommand.cs
+++ src/Silk/Commands/ThrowCommand.cs
@@ -1,13 +1,17 @@
 using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Remora.Commands.Attributes;
 using Remora.Commands.Groups;
 using Remora.Results;
+using Silk.Commands.Conditions;
 
 namespace Silk.Commands;
 
 public class ThrowCommand : CommandGroup
 {
     [Command("throw")]
+    [RequireTeamOrOwner]
+    [Description("A command mainly used for debugging.")]
     public Task<IResult> ThrowAsync() => throw new();
 }
