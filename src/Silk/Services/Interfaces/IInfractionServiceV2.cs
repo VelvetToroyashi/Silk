cc41d681b4cafafa1f36f27f9d4c892cc50ea89b
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-03-30 03:20:39 -0400

    German quality implementation, truly.

--- src/Silk/Services/Interfaces/IInfractionServiceV2.cs
+++ src/Silk/Services/Interfaces/IInfractionServiceV2.cs
@@ -1,7 +1,15 @@
+using System.Threading.Tasks;
+using Remora.Results;
+using Silk.Data.Entities;
+
 namespace Silk.Services.Interfaces;
 
 public interface IInfractionServiceV2
 {
-    
-    
+    /// <summary>
+    /// Queues an infraction to be handled.
+    /// </summary>
+    /// <param name="request">The request to queue.</param>
+    /// <returns>A task that completes when the infraction is processed.</returns>
+    public Task<Result<InfractionEntity>> SubmitInfractionAsync(InfractionRequest request);
 }
