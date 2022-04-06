3ac0fe296032397ac95d7639c754f92ac4f8fa20
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-04-06 04:50:19 -0400

    Add TimeHelper.cs

---
+++ src/Silk/Utilities/TimeHelper.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Recognizers.Text.DateTime.Wrapper;
+using Recognizers.Text.DateTime.Wrapper.Models.BclDateTime;
+
+namespace Silk.Utilities;
+
+public static class TimeHelper
+{
+    public static DateTimeOffset? Extract(string input) => ExtractImpl(input);
+
+    public static DateTimeOffset? DetectTime(this string input) => Extract(input);
+
+    private static DateTimeOffset? ExtractImpl(string input)
+    {
+        var parsedTimes = DateTimeV2Recognizer.RecognizeDateTimes(input, CultureInfo.InvariantCulture.DisplayName, DateTime.UtcNow);
+
+        if (parsedTimes.FirstOrDefault() is not { } parsedTime || !parsedTime.Resolution.Values.Any())
+            return null;
+        
+        var currentYear = DateTime.UtcNow.Year;
+
+        var timeModel = parsedTime
+                       .Resolution
+                       .Values
+                       .Where(v => v is DateTimeV2Date or DateTimeV2DateTime)
+                       .FirstOrDefault
+                            (
+                             v => v is DateTimeV2Date dtd
+                                 ? dtd.Value.Year                        >= currentYear
+                                 : (v as DateTimeV2DateTime)!.Value.Year >= currentYear
+                            );
+        
+        return (timeModel as DateTimeV2Date)?.Value ?? (timeModel as DateTimeV2DateTime)?.Value;
+    }
+    
+    private static class MicroTimeParser
+    {
+        private static readonly Regex _timeRegex = new(@"(?<quantity>\d+)(?<unit>mo|[ywdhms])", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public static TimeSpan? TryParse(string input)
+        {
+            var start = TimeSpan.Zero;
+        
+            var matches = _timeRegex.Matches(input);
+
+            if (!matches.Any())
+                return null;
+
+            var returnResult = matches.Aggregate(start, (c, n) =>
+            {
+                var multiplier = int.Parse(n.Groups["quantity"].Value);
+                var unit       = n.Groups["unit"].Value;
+
+                return c + unit switch
+                {
+                    "mo" => TimeSpan.FromDays(30  * multiplier),
+                    "y"  => TimeSpan.FromDays(365 * multiplier),
+                    "w"  => TimeSpan.FromDays(7   * multiplier),
+                    "d"  => TimeSpan.FromDays(multiplier),
+                    "h"  => TimeSpan.FromHours(multiplier),
+                    "m"  => TimeSpan.FromMinutes(multiplier),
+                    "s"  => TimeSpan.FromSeconds(multiplier),
+                    _    => TimeSpan.Zero
+                };
+            });
+
+            if (returnResult == TimeSpan.Zero)
+                return null;
+        
+            return returnResult;
+        }
+    }
+}
