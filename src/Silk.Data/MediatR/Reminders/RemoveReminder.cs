6e99d572809d7630a65c6a55fa6ba9600859d9b5
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-05-31 02:31:09 -0400

    feat: fix SBP-20

--- src/Silk.Data/MediatR/Reminders/RemoveReminder.cs
+++ src/Silk.Data/MediatR/Reminders/RemoveReminder.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Remora.Results;
 using Silk.Data.Entities;
 
 namespace Silk.Data.MediatR.Reminders;
@@ -11,23 +12,27 @@ public static class RemoveReminder
     /// <summary>
     /// Request to remove a reminder.
     /// </summary>
-    public sealed record Request(int ReminderId) : IRequest;
+    public sealed record Request(int ReminderId) : IRequest<Result>;
 
     /// <summary>
     /// The default handler for <see cref="Request" />.
     /// </summary>
-    internal sealed class Handler : IRequestHandler<Request>
+    internal sealed class Handler : IRequestHandler<Request, Result>
     {
         private readonly GuildContext _db;
 
         public Handler(GuildContext db) => _db = db;
 
-        public async Task<Unit> Handle(Request request, CancellationToken cancellationToken)
+        public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
         {
             ReminderEntity? reminder = await _db.Reminders
                                                 .FirstOrDefaultAsync(r => r.Id == request.ReminderId, cancellationToken);
 
-            if (reminder is not null)
+            if (reminder is null)
+            {
+                return Result.FromError(new NotFoundError($"A reminder with the ID of {request.ReminderId} does not exist."));
+            }
+            else
             {
                 _db.Reminders.Remove(reminder);
                 try
@@ -38,7 +43,7 @@ public static class RemoveReminder
                 catch (DbUpdateConcurrencyException) { }
             }
 
-            return new();
+            return Result.FromSuccess();
         }
     }
 }
