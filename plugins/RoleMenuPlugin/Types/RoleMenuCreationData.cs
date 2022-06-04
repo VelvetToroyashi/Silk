e43cd837d1920f95a53c2b11c5e7fa3cfd118163
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-06-04 04:23:07 -0400

    Remove unused types

--- plugins/RoleMenuPlugin/Types/RoleMenuCreationData.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using Remora.Rest.Core;
-using RoleMenuPlugin.Database;
-
-namespace RoleMenuPlugin.Types;
-
-public record RoleMenuCreationData(Snowflake UserID, Snowflake GuildID, Snowflake MessageID, RoleMenuOptionModel? Current, IReadOnlyList<RoleMenuOptionModel> Options);
