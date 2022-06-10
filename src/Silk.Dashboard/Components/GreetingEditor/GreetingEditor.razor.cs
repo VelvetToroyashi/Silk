ecad2579c27c4abebc4b692c85e6fb9ee3c8e73d
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2022-06-09 23:54:50 -0400

    Small update to Dashboard GreetingEditor component + add test page

--- src/Silk.Dashboard/Components/GreetingEditor/GreetingEditor.razor.cs
+++ src/Silk.Dashboard/Components/GreetingEditor/GreetingEditor.razor.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using MudBlazor;
+using Remora.Rest.Core;
 using Silk.Data.Entities;
+using Silk.Data.MediatR.Guilds;
 
 namespace Silk.Dashboard.Components;
 
 public partial class GreetingEditor
 {
-    private const int    MaxMessageLength   = 2000;
+    private const int MaxMessageLength = 2000;
+
     private const string MetaDataIdHelpText = "The ID of the metadata to use for contextual greeting." +
                                               "In the case of greeting when the user receives a role, this will be the ID of the role to check for before greeting." +
                                               "In the case of greeting when a user gains access to a new channel, this will be the ID of the channel to check before greeting.";
@@ -15,11 +20,52 @@ public partial class GreetingEditor
 
     private readonly GreetingOption[] _greetingOptions = Enum.GetValues<GreetingOption>();
 
+    [Inject]
+    public IMediator Mediator { get; set; }
+
+    [Inject]
+    public ISnackbar Snackbar { get; set; }
+
     [Parameter]
     public GuildGreetingEntity Greeting { get; set; } = new();
 
+    private bool ExistingGreetingFound(GuildConfigEntity config, out GuildGreetingEntity existingGreeting)
+    {
+        existingGreeting = config.Greetings.FirstOrDefault(g => g.ChannelID == Greeting.ChannelID);
+        return existingGreeting != null;
+    }
+
     private async Task SubmitAsync()
     {
-        // await ComponentRunAsync()
+        await ComponentRunAsync
+            (
+             async () =>
+             {
+                 var config = await Mediator.Send(new GetGuildConfig.Request(Greeting.GuildID));
+
+                 var foundGreeting = ExistingGreetingFound(config, out var existingGreeting);
+
+                 if (foundGreeting)
+                 {
+                     existingGreeting.GuildID    = Greeting.GuildID;
+                     existingGreeting.ChannelID  = Greeting.ChannelID;
+                     existingGreeting.Message    = Greeting.Message;
+                     existingGreeting.Option     = Greeting.Option;
+                     existingGreeting.MetadataID = Greeting.MetadataID;
+
+                     await Mediator.Send(new UpdateGuildConfig.Request(Greeting.GuildID, config.Greetings));
+                     Snackbar.Add($"Updated greeting with ID {Greeting.Id}`", Severity.Success);
+                 }
+
+                 // Creating a Greeting
+                 else
+                 {
+                     config.Greetings.Add(Greeting);
+
+                     await Mediator.Send(new UpdateGuildConfig.Request(Greeting.GuildID, config.Greetings));
+                     Snackbar.Add($"Created greeting with ID {Greeting.Id}`", Severity.Success);
+                 }
+             }
+            );
     }
 }
