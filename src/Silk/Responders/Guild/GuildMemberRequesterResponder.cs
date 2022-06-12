fd7b7617d29244026172777d404eb3b3769855e6
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-06-11 22:45:41 -0400

    🐛 Limit member requests to ~120/60s

--- src/Silk/Responders/Guild/GuildMemberRequesterResponder.cs
+++ src/Silk/Responders/Guild/GuildMemberRequesterResponder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Remora.Discord.API.Abstractions.Gateway.Events;
@@ -10,6 +11,11 @@ namespace Silk.Responders;
 
 public class GuildMemberRequesterResponder : IResponder<IGuildCreate>
 {
+    private static readonly SemaphoreSlim  _sync = new(1);
+    private static          DateTimeOffset _last = DateTimeOffset.UtcNow;
+
+    private static readonly TimeSpan _minimum = TimeSpan.FromMilliseconds(500);
+    
     private readonly DiscordGatewayClient _client;
     public GuildMemberRequesterResponder(DiscordGatewayClient client) => _client = client;
 
@@ -17,6 +23,14 @@ public class GuildMemberRequesterResponder : IResponder<IGuildCreate>
     {
         if (gatewayEvent.IsUnavailable.IsDefined(out var unavailable) && unavailable)
             return Result.FromSuccess(); // Thanks, Night.
+
+        await _sync.WaitAsync(ct);
+
+        var now = _last;
+        _last = DateTimeOffset.UtcNow;
+
+        if (now - _last < _minimum)
+            await Task.Delay(_minimum - (now - _last), ct);
         
         _client.SubmitCommand(new RequestGuildMembers(gatewayEvent.ID, limit:0));
         
