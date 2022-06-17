91427656a5434dec0364dda85abfac0bdde82c1e
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-06-16 23:53:51 -0400

    add upsert lib

--- src/Silk.Data/MediatR/Users/BulkAddUserToGuild.cs
+++ src/Silk.Data/MediatR/Users/BulkAddUserToGuild.cs
@@ -37,28 +37,12 @@ public static class BulkAddUserToGuild
         public async Task<Unit> Handle(Request request, CancellationToken  cancellationToken)
         {
             await using var trans = await _db.Database.BeginTransactionAsync(cancellationToken);
+
+            var users      = request.Users.Select(u => new UserEntity() { ID          = u.ID, History = new() { new() { UserID = u.ID, GuildID = request.GuildID, JoinDate = u.JoinedAt } } });
+            var guildUsers = request.Users.Select(u => new GuildUserEntity() { UserID = u.ID, GuildID = request.GuildID });
             
-            var users      = await _db.Users.AsNoTracking().Select(u => u.ID).ToArrayAsync(cancellationToken);
-            var usersToAdd = request.Users.ExceptBy(users, u => u.ID);
-
-            if (usersToAdd.Any())
-                _db.Users.AddRange(usersToAdd.Select(u => new UserEntity() { ID = u.ID, History = new() { new() { GuildID = request.GuildID, JoinDate = u.JoinedAt } } }));
-
-            var guildUsers = await _db.GuildUsers
-                                 .AsNoTracking()
-                                 .Where(gu => gu.GuildID == request.GuildID)
-                                 .Select(gu => gu.UserID)
-                                 .ToArrayAsync(cancellationToken);
-
-            var guildUsersToAdd = request.Users.ExceptBy(guildUsers, u => u.ID);
-            
-            Console.WriteLine($"User Count: {users.Length}, Users to add: {usersToAdd.Count()} | Guild users: {guildUsers.Length}, Guild users to add: {guildUsersToAdd.Count()}");
-            
-            if (guildUsersToAdd.Any())
-                _db.GuildUsers.AddRange(guildUsersToAdd.Select(u => new GuildUserEntity { GuildID = request.GuildID, UserID = u.ID }));
-
-            if (usersToAdd.Any() || guildUsersToAdd.Any())
-                await _db.SaveChangesAsync(cancellationToken);
+            await _db.Users.UpsertRange(users).NoUpdate().RunAsync(cancellationToken);
+            await _db.GuildUsers.UpsertRange(guildUsers).NoUpdate().RunAsync(cancellationToken);
 
             await trans.CommitAsync(cancellationToken);
             
