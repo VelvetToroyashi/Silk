02be92d7a0989e494f87ca98793f62b965f53677
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-06-20 15:17:13 -0400

    Change how users and histories get cached

--- src/Silk.Data/GuildContext.cs
+++ src/Silk.Data/GuildContext.cs
@@ -8,10 +8,12 @@ public class GuildContext : DbContext
 {
     public GuildContext(DbContextOptions<GuildContext> options) : base(options) { }
 
-    public DbSet<TagEntity>               Tags               { get; set; }
-    public DbSet<UserEntity>              Users              { get; set; }
-    public DbSet<GuildEntity>             Guilds             { get; set; }
-    public DbSet<ReminderEntity>          Reminders          { get; set; }
+    public DbSet<TagEntity>         Tags      { get; set; }
+    public DbSet<UserEntity>        Users     { get; set; }
+    public DbSet<GuildEntity>       Guilds    { get; set; }
+    public DbSet<ReminderEntity>    Reminders { get; set; }
+    public DbSet<UserHistoryEntity> Histories { get; set; }
+    
     public DbSet<GuildUserEntity>         GuildUsers         { get; set; }
     public DbSet<InfractionEntity>        Infractions        { get; set; }
     public DbSet<GuildConfigEntity>       GuildConfigs       { get; set; }
