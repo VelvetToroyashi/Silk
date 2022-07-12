e7383e368cf0b6c1d79f827cd96f599d69878c79
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-07-12 06:56:00 -0400

    Add migrations

--- src/Silk.Data/Migrations/20220712100320_Anti-Raid.Designer.cs
+++ src/Silk.Data/Migrations/20220712100320_Anti-Raid.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Silk.Data;
@@ -11,9 +12,10 @@ using Silk.Data;
 namespace Silk.Data.Migrations
 {
     [DbContext(typeof(GuildContext))]
-    partial class SilkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220712100320_Anti-Raid")]
+    partial class AntiRaid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
