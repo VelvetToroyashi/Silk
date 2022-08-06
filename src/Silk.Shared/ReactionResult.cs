ea4a742ffbd0b621082e456ffca9ca050811cd5d
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-08-06 19:44:00 -0400

    Make ReactionResult an error for convenience

--- src/Silk.Shared/ReactionResult.cs
+++ src/Silk.Shared/ReactionResult.cs
@@ -1,5 +1,6 @@
 using OneOf;
 using Remora.Rest.Core;
+using Remora.Results;
 
 namespace Silk.Shared;
 
@@ -8,4 +9,7 @@ namespace Silk.Shared;
 /// </summary>
 /// <param name="Reaction">The ID of the reaction, or its unicode representation.</param>
 /// <param name="Message">The optional message to send.</param>
-public record ReactionResult(OneOf<Snowflake, ulong, string> Reaction, Optional<string> Message = default);
+public record ReactionResult(OneOf<Snowflake, ulong, string> Reaction, Optional<string> Message = default) : IResultError
+{
+    string IResultError.Message => Message.IsDefined(out var message) ? message : string.Empty;
+}
