20d164a76f4702b9b8d9382ebe4ef76e4a247338
Caleb A. Bostic-Gardner <13528490+CalebABG@users.noreply.github.com>
2022-08-07 03:55:39 -0400

    Remove `HttpContextAccessor` service from DI

--- src/Silk.Dashboard/DependencyInjection.cs
+++ src/Silk.Dashboard/DependencyInjection.cs
@@ -175,7 +175,6 @@ public static class DependencyInjection
     )
     {
         services.AddMemoryCache();
-        services.AddHttpContextAccessor();
 
         services.AddHttpClient("Github", con => 
         {
