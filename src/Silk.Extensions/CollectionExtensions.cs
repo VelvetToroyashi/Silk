d48a3a46a13e4ac67b70075aaa19ba2d84ab6be7
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-09-16 02:03:40 -0400

    Use `Array.IndexOf` and remove extension method

--- src/Silk.Extensions/CollectionExtensions.cs
+++ src/Silk.Extensions/CollectionExtensions.cs
@@ -10,15 +10,6 @@ namespace Silk.Extensions;
 
 public static class CollectionExtensions
 {
-    /// <summary>Returns the index of an element contained in a list if it is found, otherwise returns -1.</summary>
-    public static int IndexOf<T>(this IReadOnlyList<T> list, T element) // IList doesn't implement IndexOf for some reason
-    {
-        for (var i = 0; i < list.Count; i++)
-            if (list[i]?.Equals(element) ?? false)
-                return i;
-        return -1;
-    }
-
     /// <summary>Fluid method that joins the members of a collection using the specified separator between them.</summary>
     public static string Join<T>(this IEnumerable<T> values, string separator = "") => string.Join(separator, values);
 
