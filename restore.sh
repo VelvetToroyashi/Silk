e423c39a9b604b52571f0839089e35f8356207f3
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2023-02-27 20:26:54 -0500

    a

--- restore.sh
+++ restore.sh
@@ -5,3 +5,12 @@ for i in ./src/*; do
         dotnet restore "$i"/*.csproj 1> /dev/null
     fi
 done
+
+echo "Setting architecture variable for dotnet..."
+if [ "$ARCH" = "amd64" ]; then {
+  echo "Detected 'amd64' architecture, setting 'x64'..."
+  export BUILD_ARCH=x64
+} else {
+  echo "Detected 'arm64v8' architecture, setting 'arm64'..."
+  export BUILD_ARCH=arm64;
+}  fi
